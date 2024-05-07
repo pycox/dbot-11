5bd65bf5659f3c73363408b2f722f3dd857b2076
Shiva <shivanand.gupta@outlook.com>
2024-05-07 23:12:44 +0530

    Created the scraper 310

---
+++ scripts/scraper310.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 310
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "article.job-entry")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a.job-entry__link").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".job-entry__info .job-entry__meta.job-location").text.strip()
+        location_ = location.lower()
+        for str in ['london', 'new york', 'san francisco', 'united states', 'united Kingdom', 'uk', 'usa', 'us']:
+            if (str in location_):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".job-entry__title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
