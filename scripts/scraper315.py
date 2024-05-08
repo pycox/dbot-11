84479f15ef4e75e40a7f26efe0a57a87083ed4f0
Shiva <shivanand.gupta@outlook.com>
2024-05-08 07:09:18 +0530

    Created the scraper 315

---
+++ scripts/scraper315.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 315
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, "button#onetrust-accept-btn-handler").click()
+    except:
+        print("No Cookie Button")
+
+    Select(driver.find_element(By.CSS_SELECTOR, "div:nth-child(2) > div > select")).select_by_value("4052641002,4052642002,4052644002")
+    
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "div.grid__item.careers-listing__card[data-visible='true']")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                com,
+                "United Kingdom",
+                link,
+            ]
+        )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
