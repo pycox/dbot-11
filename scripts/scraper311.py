6189c6911540e7249aa364b3d41b5f1bab5e3646
Shiva <shivanand.gupta@outlook.com>
2024-05-08 06:39:15 +0530

    Created the scraper 311

---
+++ scripts/scraper311.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 311
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(10)
+
+    data = []
+    
+    flag = True
+    while flag:
+        iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "icims_content_iframe")))
+        driver.switch_to.frame(iframe)
+        items = driver.find_elements(By.CSS_SELECTOR, ".iCIMS_JobsTable .row")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".iCIMS_JobHeaderData").text.strip()
+            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
+
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'a[title="Next page of results"]').click()
+            time.sleep(8)
+        except:
+            flag = False
+            print("No More Jobs")
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
