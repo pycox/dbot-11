191bad9c6cfc43d0202b4875a388bfbdc57db91f
Shiva <shivanand.gupta@outlook.com>
2024-05-09 23:18:09 +0530

    Created the scraper 371

---
+++ scripts/scraper371.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 371
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button[data-cy="agree-to-selected-cookies"]').click()
+    except:
+        print("No Cookie Button")
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".sc-6exb5d-0.eMaJjq")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".sc-qfruxy-6.fcCfmW.custom-css-style-job-location").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
