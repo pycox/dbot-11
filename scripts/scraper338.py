e54c24cbc1cd2579fdba7764540f6a97c05fe9df
Shiva <shivanand.gupta@outlook.com>
2024-05-09 12:01:07 +0530

    Created the scraper 338

---
+++ scripts/scraper338.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 338
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    data = []
+
+    flag = True
+
+    while flag:
+        try:
+            time.sleep(4)
+
+            driver.find_element(
+                By.CSS_SELECTOR, 'button[data-ui="load-more-button"]'
+            ).click()
+
+        except Exception as e:
+            flag = False
+            break    
+
+    items = driver.find_elements(By.CSS_SELECTOR, "li[data-ui='job-opening']")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
