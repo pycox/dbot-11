24e59fe9d42d3e2775e7dc45216655831acf5f19
Shiva <shivanand.gupta@outlook.com>
2024-05-09 11:50:36 +0530

    Created the scraper 336

---
+++ scripts/scraper336.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 336
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+    
+    driver.execute_script("document.querySelectorAll('.career-positions-slider-swiper').forEach(e => e.classList.remove('swiper'));")
+    
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".career-positions-slider-swiper .swiper-slide")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = "UK"
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, ".content-wrapper h2").text.strip(),
+                com,
+                location,
+                link,
+            ]
+        )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
