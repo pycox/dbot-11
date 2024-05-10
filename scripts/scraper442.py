9633cce082dd3792dde41b2dacb0a9d708014a81
Shiva <shivanand.gupta@outlook.com>
2024-05-11 01:13:08 +0530

    Created the scraper 442

---
+++ scripts/scraper442.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 442
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, "a#wt-cli-accept-btn").click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(4)
+    
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "#lever-jobs-container li.lever-job")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "span").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
