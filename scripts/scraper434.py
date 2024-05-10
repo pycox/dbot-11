1f02522c12e0b04d6e4434678b0d4044e6760ece
Shiva <shivanand.gupta@outlook.com>
2024-05-11 00:31:17 +0530

    Created the scraper 434

---
+++ scripts/scraper434.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 434
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(2)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(2)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".careers-job-list .careers-table-item")
+    for item in items:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".rb-paragraph-regular.rb-text-right-tablet-onwards.rb-no-margin").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".rb-careers-item-link").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
