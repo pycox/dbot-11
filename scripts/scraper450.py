5d3a4d1a5c97e57f75511d14de543f0fd668011e
Shiva <shivanand.gupta@outlook.com>
2024-05-11 01:32:29 +0530

    Created the scraper 450!

---
+++ scripts/scraper450.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 450
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    
+    time.sleep(4)
+
+    data = []
+
+    # items = driver.find_elements(By.CSS_SELECTOR, ".job-post")
+
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+    #     location = item.find_element(By.CSS_SELECTOR, ".office").text.strip()
+    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+    #         if (str in location):
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, ".name").text.strip(),
+    #                     com,
+    #                     "UK",
+    #                     link,
+    #                 ]
+    #             )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
