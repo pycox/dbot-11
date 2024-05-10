6216bca94ec17770a957a2e3ab36b361e694eb44
Shiva <shivanand.gupta@outlook.com>
2024-05-10 08:59:27 +0530

    Created the scraper 400

---
+++ scripts/scraper400.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 400
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".container.py-10.undefined ul li")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".overline-title").text.strip()
+        location_ = location.lower()
+        for str in ['london', 'new York', 'san francisco', 'united states', 'united kingdom', 'uk', 'usa', 'us']:
+            if (str in location_):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
