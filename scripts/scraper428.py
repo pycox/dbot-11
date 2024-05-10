dcc907e352075d928baf1f58d177013bce37a998
Shiva <shivanand.gupta@outlook.com>
2024-05-10 23:53:50 +0530

    Created the scraper 428

---
+++ scripts/scraper428.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 428
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+        
+    items = driver.find_elements(By.CSS_SELECTOR, ".downloadTable__tableRow .row.align-items-center")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = "London"
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, ".col-md-4.col-12").text.strip(),
+                com,
+                location,
+                link,
+            ]
+        )
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
