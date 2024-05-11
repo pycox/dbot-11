b1e7753b43b6045b668d3ee762a14bbbe51d444c
Shiva <shivanand.gupta@outlook.com>
2024-05-11 16:01:16 +0530

    Created the scraper 494

---
+++ scripts/scraper494.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 494
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".block-hiring .row .col-md-12 a")
+    for item in items:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".second").text.strip()
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, ".first").text.strip(),
+                com,
+                f"{location}, UK",
+                link,
+            ]
+        )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
