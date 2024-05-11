18efcbb13dfdfefcc6d958c78740caa466d5afea
Shiva <shivanand.gupta@outlook.com>
2024-05-11 10:15:12 +0530

    Created the scraper 463

---
+++ scripts/scraper463.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 463
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".opportunities-item")
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        location = item.find_element(By.CSS_SELECTOR, "span").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
