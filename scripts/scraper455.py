bfbda1b0dd60f226b607c35327fc674b5edb2069
Shiva <shivanand.gupta@outlook.com>
2024-05-11 09:36:24 +0530

    Created the scraper 455

---
+++ scripts/scraper455.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 455
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(2)
+
+    data = []
+    
+    # items = driver.find_elements(By.CSS_SELECTOR, "article.js--careers-card")
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    #     location = item.find_element(By.CSS_SELECTOR, ".js--career-location").text.strip()
+    #     for str in ['England', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+    #         if (str in location):
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+    #                     com,
+    #                     location,
+    #                     link,
+    #                 ]
+    #             )
+    #             break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
