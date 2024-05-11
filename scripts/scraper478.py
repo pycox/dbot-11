a264e26f54447a97014f254cc50d0b8a3e4c94e4
Shiva <shivanand.gupta@outlook.com>
2024-05-11 14:26:15 +0530

    Created the scraper 478

---
+++ scripts/scraper478.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 478
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button#epdrejectall').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".vsr-job-big")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "h2 a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "div[data-id='div_content_VacV_LocationID'] span").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
