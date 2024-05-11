caaa309f3c7cb1c780565ccc5e76ed655105b4fa
Shiva <shivanand.gupta@outlook.com>
2024-05-11 14:16:07 +0530

    Created the scraper 477

---
+++ scripts/scraper477.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 477
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".content-panel-container .content-panel.contact-block")
+
+    data = []
+
+    for item in items:
+        location = item.find_element(By.CSS_SELECTOR, '.contact-header-pnl p').text.strip()
+        if location not in ["New York", "Los Angeles", "London"]:
+            continue
+        sub_items = item.find_elements(By.CSS_SELECTOR, ".job-listings-block a")
+        for sub_item in sub_items:
+            data.append(
+                [
+                    sub_item.text.strip(),
+                    com,
+                    location,
+                    sub_item.get_attribute("href").strip(),
+                ]
+            )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
