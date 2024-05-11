faa5b5fdea483ecb7a3d894aa2afbfb4b19eca14
Shiva <shivanand.gupta@outlook.com>
2024-05-11 10:57:04 +0530

    Created the scraper 465

---
+++ scripts/scraper465.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 465
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(6)
+
+    data = []
+    
+    for region in ["United Kingdom", "United States"]:
+        driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, f'div#CountryFacet label input[aria-label="{region}"]'))
+        time.sleep(4)
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, "#j-careers-search__results .c-careers-search__list-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".location-field p").text.strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h6").text.strip(),
+                    com,
+                    location,
+                    link,
+                ]
+            )
+
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'a.c-careers-search__page-next').click()
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
