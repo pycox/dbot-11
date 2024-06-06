9e35be6d2e06679a521fef2b3b3594cab0578742
Shiva <shivanand.gupta@outlook.com>
2024-06-06 23:15:55 +0530

    Created the scraper 551

---
+++ scripts/scraper551.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 551
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".srJobList tr")
+    print(items)
+    for item in items[1:]:
+        print(item)
+        link = item.get_attribute("onclick").strip()[13:-3]
+        print(link)
+        location = item.find_element(By.CSS_SELECTOR, ".srJobListLocation").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".srJobListJobTitle").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
