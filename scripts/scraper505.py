56d233021b5f578ca7502253eac795f02022eb38
Shiva <shivanand.gupta@outlook.com>
2024-06-06 07:17:20 +0530

    Created the scraper 505

--- scripts/scraper505.py
+++ scripts/scraper505.py
@@ -14,26 +14,46 @@ def main():
     driver.get(url)
 
     time.sleep(4)
-
-
-
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button.cc-nb-okagree').click()
+        time.sleep(4)
+    except:
+        print("No Cookiee") 
+        
     data = []
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job-search-section__result a.job-card")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".job-card__list li:nth-child(1)").text.strip()
+            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "li.BambooHR-ATS-Jobs-Item")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        try:
+            next_button = driver.find_element(By.CSS_SELECTOR, '.pagination__btn--next')
+            if "is-disabled" in next_button.get_attribute("class"):
+                flag = False
+            else:
+                driver.execute_script("arguments[0].click();", next_button)
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
-    #     data.append(
-    #         [
-    #             item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-    #             com,
-    #             item.find_element(By.CSS_SELECTOR, "span").text.strip(),
-    #             link,
-    #         ]
-    #     )
 
-    # driver.quit()
-
+    driver.quit()
     updateDB(key, data)
 
 
