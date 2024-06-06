c9ab027d2a39f66ccdcf54504a79e42b8e622624
Shiva <shivanand.gupta@outlook.com>
2024-06-06 07:16:16 +0530

    Created the scraper 504

--- scripts/scraper504.py
+++ scripts/scraper504.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
@@ -14,51 +15,28 @@ def main():
     driver.get(url)
 
     time.sleep(4)
-    
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'button#cookie-accept').click()
-        time.sleep(4)
-    except:
-        print("No Cookiee") 
-        
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'a[title="Find experienced jobs"]').click()
-        time.sleep(4)
-    except:
-        print("No Page") 
-           
+
     data = []
-
-    total_jobs = driver.find_element(By.CSS_SELECTOR, 'div#content b:nth-child(2)').text.strip()
-    total_jobs = int(total_jobs)
-    current_jobs = 25
-
-    while current_jobs < total_jobs:
-        items = driver.find_elements(By.CSS_SELECTOR, "tr.data-row")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, 'span.jobLocation').text.strip()
-            title = item.find_element(By.CSS_SELECTOR, "a").text.strip()
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US', 'GB']:
-                if (str in location):
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".seo-pages-100b19y-StyledLocationAndJobs.e2oznfq0")
+    for item in items:
+        location = item.find_element(By.CSS_SELECTOR, ".seo-pages-1va6nd7-StyledText").text.strip()
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US', 'CA', 'WA', 'TX']:
+            if (str in location):
+                subitems = driver.find_elements(By.CSS_SELECTOR, ".seo-pages-1cxckch-StyledUrlWrapper.epip1yb0")
+                for subitem in subitems:
                     data.append(
                         [
-                            title,
+                            subitem.find_element(By.CSS_SELECTOR, "a").text.strip(),
                             com,
                             location,
-                            link,
+                            subitem.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip(),
                         ]
                     )
-                    break
-        
-        current_jobs += 25
-        if current_jobs < total_jobs:
-            driver.get(f"{url}ey/search/?q=&sortColumn=referencedate&sortDirection=desc&startrow={current_jobs}")
-            time.sleep(4)
+                break
 
 
     driver.quit()
-
     updateDB(key, data)
 
 
