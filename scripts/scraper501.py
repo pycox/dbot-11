66704f31cb5f0d295a2f74c42c6f41d1f7bbc037
Shiva <shivanand.gupta@outlook.com>
2024-06-06 06:11:04 +0530

    Created the scraper 501!

--- scripts/scraper501.py
+++ scripts/scraper501.py
@@ -14,29 +14,28 @@ def main():
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    # time.sleep(4)
 
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'a[title="Search vacancies"]').click()
-        time.sleep(6)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    except:
-        print("No Cookie Button")
+    # try:
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     time.sleep(3)
+    # except:
+    #     print("No Cookie Button")
 
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, "#searchResultsJS tr td a")
-    for item in items:
-        link = item.get_attribute("href").strip()
-        data.append(
-            [
-                item.text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+    # items = driver.find_elements(By.CSS_SELECTOR, ".vacancies-container .vacancy")
+    # for item in items:
+    #     link = item.get_attribute("href").strip()
+    #     data.append(
+    #         [
+    #             item.text.strip(),
+    #             com,
+    #             "UK",
+    #             link,
+    #         ]
+    #     )
 
 
     driver.quit()
