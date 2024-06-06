e01a5fcab21cb07a7c90b88367556be8822aed0e
Shiva <shivanand.gupta@outlook.com>
2024-06-06 09:51:30 +0530

    Created the scraper 512#

--- scripts/scraper512.py
+++ scripts/scraper512.py
@@ -14,25 +14,23 @@ def main():
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, ".job-list li")
-    for item in items:
-        link = f"{url}?jobId={item.get_attribute('id')}"
-        location = item.find_element(By.CSS_SELECTOR, ".location-info").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, ".job-title").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    # items = driver.find_elements(By.CSS_SELECTOR, ".job-list li")
+    # for item in items:
+    #     link = f"{url}?jobId={item.get_attribute('id')}"
+    #     location = item.find_element(By.CSS_SELECTOR, ".location-info").text.strip()
+    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+    #         if (str in location):
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, ".job-title").text.strip(),
+    #                     com,
+    #                     location,
+    #                     link,
+    #                 ]
+    #             )
+    #             break
 
 
     driver.quit()
