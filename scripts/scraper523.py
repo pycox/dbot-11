425cb9822344329460b68cfc56639d3243a00525
Shiva <shivanand.gupta@outlook.com>
2024-06-06 11:25:02 +0530

    Created the scraper 523

---
+++ scripts/scraper523.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 523
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".job-card")
+    print(len(items))
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        data.append(
+            [
+                driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, ".job-card__title .h4")),
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
