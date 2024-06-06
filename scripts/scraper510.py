a68e03e6b3a2ebb3adc4a6504a5c14bc38be57d6
Shiva <shivanand.gupta@outlook.com>
2024-06-06 09:33:23 +0530

    Created the scraper 510

--- scripts/scraper510.py
+++ scripts/scraper510.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
-import math
 
 
 def main():
@@ -14,56 +14,23 @@ def main():
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#cookie-accept",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
-
     time.sleep(2)
 
-
-    driver.find_element(By.CSS_SELECTOR, ".keywordsearchbutton").click()
-    time.sleep(4)
-
-    total = int(
-        driver.find_element(
-            By.CSS_SELECTOR, ".paginationLabel b:last-child"
-        ).text.strip()
-    )
-
     data = []
-
-    for i in range(0, math.floor(total / 25)):
-        if i != 0:
-            driver.get(
-                f"https://jobs.knightfrank.com/search/?q=&sortColumn=referencedate&sortDirection=desc&searchby=location&d=15&startrow={i * 25}"
-            )
-
-        time.sleep(4)
-
-        items = driver.find_elements(By.CSS_SELECTOR, "tr.data-row")
-
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, "td.colLocation").text.strip()
-
-            if location.split(",")[1].strip() in ["GB", "US"]:
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "td.colTitle").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".text-image-2__content.flex.justify-center")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
