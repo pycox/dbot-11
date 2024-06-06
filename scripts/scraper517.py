b4399d809623fbaa0ab73beede617c8010c25f22
Shiva <shivanand.gupta@outlook.com>
2024-06-06 10:16:08 +0530

    Created the scraper 517

---
+++ scripts/scraper517.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 517
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(2)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "#jobs-section a.sc-4226180-1.tDrtJ")
+    for item in items:
+        link = item.get_attribute("href").strip()
+        data.append(
+            [
+                item.text.strip(),
+                com,
+                "United Kingdom",
+                link,
+            ]
+        )
+
+    driver.quit()
+    print(key, data)
+
+
+if __name__ == "__main__":
+    main()
