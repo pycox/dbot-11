11d3027d572617043c7ff6ba3716d5fe0409b4eb
Shiva <shivanand.gupta@outlook.com>
2024-06-06 23:29:51 +0530

    Created the scraper 553

---
+++ scripts/scraper553.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 553
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".opening")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
