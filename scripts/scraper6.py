d7b4d09dabd4ab0cead22c71ddb8f8126dcf7f26
Shiva <shivanand.gupta@outlook.com>
2024-07-23 11:46:30 +0530

    Update the scraper 6

--- scripts/scraper6.py
+++ scripts/scraper6.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 6
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,20 +13,22 @@ def main():
     
     time.sleep(10)
     
-    items = driver.find_element(By.CSS_SELECTOR, 'div.jobnav').find_elements(By.CSS_SELECTOR, "li")
+    items = driver.find_elements(By.CSS_SELECTOR, 'div.posting')
     
     data = []
     
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "h4").text.strip()
-            
-        data.append([
-            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-            com,
-            location,
-            link
-        ])
+        location = item.find_element(By.CSS_SELECTOR, "span.location").text.strip()
+        for str in locations:
+            if str in location:
+                data.append([
+                    item.find_element(By.CSS_SELECTOR, "h5").text.strip(),
+                    com,
+                    location,
+                    link
+                ])
+                break
                 
     updateDB(key, data)
 
