67982d2a4d4ae78f67381542f7bc8de53c9cb147
Shiva <shivanand.gupta@outlook.com>
2024-07-23 11:46:37 +0530

    Update the scraper 7

--- scripts/scraper7.py
+++ scripts/scraper7.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 7
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -34,14 +32,15 @@ def main():
             for item in items:
                 link = item.find_element(By.CSS_SELECTOR, "a").get_attribute('href')
                 location = item.find_element(By.CSS_SELECTOR, "span[itemprop='address']").text.strip().split(',')[-1].strip()
-                
-                if location in ['United States', 'United Kingdom']:
-                    data.append([
-                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                        com,
-                        location,
-                        link
-                    ])
+                for str in locations:
+                    if str in location:
+                        data.append([
+                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            com,
+                            location,
+                            link
+                        ])
+                        break
                 
                 newHist.append(link)
             
