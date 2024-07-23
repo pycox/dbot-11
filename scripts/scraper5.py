555002178d6b44d62d470dbf4c6cc9a86d254907
Shiva <shivanand.gupta@outlook.com>
2024-07-23 11:46:24 +0530

    Update the scraper 5

--- scripts/scraper5.py
+++ scripts/scraper5.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 5
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -35,13 +33,17 @@ def main():
     
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute('href')
-                        
-        data.append([
-            item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-            com,
-            item.find_element(By.CSS_SELECTOR, 'div.sub-text').text.strip(),
-            link
-        ])
+        location  = item.find_element(By.CSS_SELECTOR, 'div.sub-text').text.strip()
+                
+        for str in locations:
+            if str in location:                
+                data.append([
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    location,
+                    link
+                ])
+                break
             
     updateDB(key, data)
     
