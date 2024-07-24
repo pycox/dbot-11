8a4e0244607d238b5715f9b16dff6ca032d5649f
Shiva <shivanand.gupta@outlook.com>
2024-07-24 09:34:47 +0530

    Update the scraper 49

--- scripts/scraper49.py
+++ scripts/scraper49.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 49
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -33,21 +31,21 @@ def main():
             flag = False
             break
 
-    items = driver.find_elements(By.CSS_SELECTOR, "li.job_listing")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.job_listing")
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        title = item.find_element(By.CSS_SELECTOR, "div.position").text.strip()
-        location = item.find_element(By.CSS_SELECTOR, "div.location").text.strip()
-
-        data.append(
-            [
-                title,
-                com,
-                location,
-                link,
-            ]
-        )
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            title = item.find_element(By.CSS_SELECTOR, "div.position").text.strip()
+            # location = item.find_element(By.CSS_SELECTOR, "div.location").text.strip()
+            data.append(
+                [
+                    title,
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
