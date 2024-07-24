4225243530fcc3dc3f1adffe25d01d89d425af3e
Shiva <shivanand.gupta@outlook.com>
2024-07-24 07:52:10 +0530

    Update the scraper 29

--- scripts/scraper29.py
+++ scripts/scraper29.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 29
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -23,9 +21,10 @@ def main():
         link = item.find_element(By.CSS_SELECTOR, 'a').get_attribute("href").strip()
         title = item.find_element(By.CSS_SELECTOR, 'a').text.strip()
         location = item.find_element(By.CSS_SELECTOR, ".vacancyListDetail.vldLocation .vacancyListDetailData").text.strip()
-        
-        data.append([title, com, location, link])
-        
+        for str in locations:
+            if str in location:
+                data.append([title, com, location, link])
+                break
     updateDB(key, data)
 
 
