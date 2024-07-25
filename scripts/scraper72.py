90cb9cc81efdcc1c4ebfa719e4d181ae791fd30b
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:39:48 +0530

    Update the scraper 72

--- scripts/scraper72.py
+++ scripts/scraper72.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 70
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -27,19 +25,20 @@ def main():
 
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.partial-strapline")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "div.partial-strapline")
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
