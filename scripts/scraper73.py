4052292b80d12d9debd715ae6578c0d56ebde7d0
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:39:53 +0530

    Update the scraper 73

--- scripts/scraper73.py
+++ scripts/scraper73.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 73
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -33,12 +31,7 @@ def main():
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
         location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
 
-        for str in [
-            "London",
-            "New York",
-            "UK",
-            "US",
-        ]:
+        for str in locations:
             if str in location:
                 data.append(
                     [
@@ -48,6 +41,7 @@ def main():
                         link,
                     ]
                 )
+                break
 
     driver.quit()
 
