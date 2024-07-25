f29b3d53649380aca17e7b33b12aa3866977e8ee
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:38:13 +0530

    Update the scraper 57

--- scripts/scraper57.py
+++ scripts/scraper57.py
@@ -5,44 +5,82 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 57
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    time.sleep(2)
 
     try:
         driver.find_element(
             By.CSS_SELECTOR,
-            "button#gdpr-button",
+            "#system-ialert-button",
         ).click()
     except Exception as e:
         print(f"Scraper{key} cookiee button: {e}")
 
     time.sleep(2)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "ul.search-results-list > li")
-
     data = []
+    regions = []
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "span.job-location").text.strip()
+    if "UK" in locations:
+        regions.append("United Kingdom")
+    if "US" in locations:
+        regions.append("United States")
+    
+    button = driver.find_element(By.CSS_SELECTOR, "#country-toggle")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(3)
+    button = driver.find_element(By.CSS_SELECTOR, f'input[data-display="{regions[0]}"]')
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(3)
+    for region in regions:
+        button = driver.find_element(By.CSS_SELECTOR, "#country-toggle")
+        driver.execute_script("arguments[0].scrollIntoView();", button)
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(3)
+        button = driver.find_element(By.CSS_SELECTOR, f'input[data-display="{region}"]')
+        driver.execute_script("arguments[0].scrollIntoView();", button)
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(3)
+        
+        flag = True
+        
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, "li.job-card")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                title = driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, ".job-card__title"))
+                
+                data.append(
+                    [
+                        title.strip(),
+                        com,
+                        region,
+                        link,
+                    ]
+                )
 
-        if location.split(",")[-1].strip() in ["United Kingdom", "United States"]:
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
-
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
+                if "disable" in next_button.get_attribute("class"):
+                    flag = False
+                else:
+                    driver.execute_script("arguments[0].click();", next_button)
+                    
+                time.sleep(4)
+            except Exception as e:
+                flag = False
+                print("No More Jobs", e)
+        
+        button = driver.find_element(By.CSS_SELECTOR, '#search-filter-clear')
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(4)
+            
     driver.quit()
 
     updateDB(key, data)
