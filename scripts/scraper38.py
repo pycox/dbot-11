42051404e7f9da441a9f3d75ca10dc1d53882c08
Shiva <shivanand.gupta@outlook.com>
2024-07-25 08:23:34 +0530

    Update the scraper 38

--- scripts/scraper38.py
+++ scripts/scraper38.py
@@ -1,48 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 38
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(8)
-
-    items = len(driver.find_elements(By.CSS_SELECTOR, "div.current-openings-item"))
+    time.sleep(4)
 
     data = []
+    
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, "#site-footer"))
+    time.sleep(4)
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "div.w-full.space-y-30 > div")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "div.w-full.flex.flex-wrap.items-center.justify-between > div:nth-child(1) > ul > li:nth-child(1) span.text-14").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    for i in range(0, items):
-        item = driver.find_elements(By.CSS_SELECTOR, "div.current-openings-item")[i]
-        
-        title = item.find_element(
-            By.CSS_SELECTOR, "span.current-opening-title"
-        ).text.strip()
-        location = item.find_element(
-            By.CSS_SELECTOR, "div.current-opening-locations"
-        ).text.strip()
 
-        item.find_element(By.CSS_SELECTOR, "button").click()
-
-        time.sleep(4)
-
-        data.append([title, com, location, driver.current_url])
-
-        driver.find_element(
-            By.CSS_SELECTOR, "button#recruitment_jobDescription_back"
-        ).click()
-
-        time.sleep(4)
-        
     driver.quit()
-
     updateDB(key, data)
 
 
