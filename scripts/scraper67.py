fe5720d59f52d3c5ac281a4f60424ab118b12d81
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:39:19 +0530

    Update the scraper 67

--- scripts/scraper67.py
+++ scripts/scraper67.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 67
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -20,21 +18,19 @@ def main():
     items = driver.find_elements(By.CSS_SELECTOR, 'li[data-ui="job"]')
 
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        location = item.find_element(By.CSS_SELECTOR, 'span[data-ui="job-location"]').text.strip()
-
-        if location.split(',')[-1].strip() in ["United Kingdom", "United State"]:
-            data.append(
-                [
-                    item.find_element(
-                        By.CSS_SELECTOR, 'h3[data-ui="job-title"]'
-                    ).text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
-
+        location = item.find_element(By.CSS_SELECTOR, 'div[data-ui="job-location"]').text.strip()
+        for str in locations:
+            if (str in location):
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, 'h3[data-ui="job-title"]').text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
     driver.quit()
 
     updateDB(key, data)
