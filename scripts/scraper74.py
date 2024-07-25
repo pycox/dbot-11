8cc48eafb3305d79d453989dd0eaa90661a23fce
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:39:58 +0530

    Update the scraper 74

--- scripts/scraper74.py
+++ scripts/scraper74.py
@@ -5,49 +5,50 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 74
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(2)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR,"#accept_all_cookies_button").click()
+    except Exception as e:
+        print(f"Scraper{key} cookiee button: {e}")
+
     time.sleep(4)
 
-    # try:
-    #     driver.find_element(
-    #         By.CSS_SELECTOR,
-    #         "button#js-cookie-accept",
-    #     ).click()
-    # except Exception as e:
-    #     print(f"Scraper{key} cookiee button: {e}")
-
-    # time.sleep(4)
-
+    driver.find_element(By.CSS_SELECTOR, "#submitSearch").click()
+    time.sleep(4)
+    
     data = []
+    flag = True
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "div.card-job")
+    if "UK" in locations:
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, "li.search-results-job-box")
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-    #     location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
-
-    #     for str in [
-    #         "London",
-    #         "New York",
-    #         "UK",
-    #         "US",
-    #     ]:
-    #         if str in location:
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        "United Kingdom",
+                        link,
+                    ]
+                )
+            
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, 'a[title="Go to next search results page"]')
+                next_button.click()
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
+        
 
     driver.quit()
 
