f0b96f86e220e59017c60e982bfe986ceebfae66
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:40:13 +0530

    Update the scraper 77

--- scripts/scraper77.py
+++ scripts/scraper77.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 77
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -27,22 +25,22 @@ def main():
 
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, "li.vacancy-item-new")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.vacancy-item-new")
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        location = item.find_element(
-            By.XPATH, "//dd[contains(text(), 'Location')]/following-sibling::dt"
-        ).text.strip()
-
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+            location = item.find_element(
+                By.XPATH, "//dd[contains(text(), 'Location')]/following-sibling::dt"
+            ).text.strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    location,
+                    link,
+                ]
+            )
         
     driver.quit()
 
