ac247062f9ce292ae70717bd21485ad77ed5da99
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:39:02 +0530

    Update the scraper 64

--- scripts/scraper64.py
+++ scripts/scraper64.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 64
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,37 +14,36 @@ def main():
     flag = True
     data = []
 
-    while flag:
-        try:
-            time.sleep(4)
+    if "UK" in locations:
+        while flag:
+            try:
+                time.sleep(4)
 
-            items = driver.find_elements(By.CSS_SELECTOR, "div.position_opening")
+                items = driver.find_elements(By.CSS_SELECTOR, "div.position_opening")
 
-            for item in items:
-                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-                location = item.find_elements(By.CSS_SELECTOR, "div.panel-body > p")[
-                    1
-                ].text.strip()
-                
-                print(item.find_element(By.CSS_SELECTOR, "h1").text.strip())
+                for item in items:
+                    link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                    location = item.find_elements(By.CSS_SELECTOR, "div.panel-body > p")[
+                        1
+                    ].text.strip()
+                    
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h1").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
 
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "h1").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
+                nextBtn = driver.find_elements(By.CSS_SELECTOR, 'a[rel="next"]')
 
-            nextBtn = driver.find_elements(By.CSS_SELECTOR, 'a[rel="next"]')
-
-            if len(nextBtn) > 0:
-                nextBtn[0].click()
-            else:
+                if len(nextBtn) > 0:
+                    nextBtn[0].click()
+                else:
+                    flag = False
+            except:
                 flag = False
-        except:
-            flag = False
 
     updateDB(key, data)
 
