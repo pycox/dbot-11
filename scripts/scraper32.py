097a11b39b9aef803d470202d3ec2fc36d45d92a
Shiva <shivanand.gupta@outlook.com>
2024-07-25 08:19:20 +0530

    Update the scraper 32!

--- scripts/scraper32.py
+++ scripts/scraper32.py
@@ -1,48 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 32
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    driver.get("https://careers.next15.com/jobs")
 
     time.sleep(4)
 
-    try:
-        driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Accept all cookies')]"
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookie Button: {e}")
-
-    time.sleep(2)
-
-    dom = driver.find_element(By.CSS_SELECTOR, "ul#jobs_list_container")
-
-    items = dom.find_elements(By.CSS_SELECTOR, "li")
-
     data = []
+    
+    # flag = True
+    # while flag:
+    #     time.sleep(4)
+    #     try:
+    #         driver.find_element(By.CSS_SELECTOR, "div.d-flex.mt-4.ng-star-inserted > button").click()
+    #     except Exception:
+    #         flag = False
+    
+    # items = driver.find_elements(By.CSS_SELECTOR, "li.careers-listings-item")
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    #     location = item.find_element(By.CSS_SELECTOR, ".careers-listings-item--location").text.strip()
+    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+    #         if (str in location):
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+    #                     com,
+    #                     location,
+    #                     link,
+    #                 ]
+    #             )
+    #             break
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "span").text.strip(),
-                com,
-                "London",
-                link,
-            ]
-        )
-
+    driver.quit()
     updateDB(key, data)
 
 
