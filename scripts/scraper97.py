3687ca74945d607c4877c3b2c9d2507e83dca8d9
Shiva <shivanand.gupta@outlook.com>
2024-07-25 20:42:02 +0530

    Update the scraper 97

--- scripts/scraper97.py
+++ scripts/scraper97.py
@@ -5,39 +5,34 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 97
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(8)
-
-    iframe = driver.find_element(By.TAG_NAME, "iframe")
-    driver.switch_to.frame(iframe)
-
-    driver.find_element(By.CSS_SELECTOR, "input[name='tbe_cws_submit']").click()
-
-    time.sleep(8)
-
-    dom = driver.find_element(By.CSS_SELECTOR, "table#cws-search-results")
-    items = dom.find_elements(By.CSS_SELECTOR, "tr")[1:]
+    time.sleep(4)
+    driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary.oracletaleocwsv2-btn-fa.fa-search").click()
+    time.sleep(4)
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".oracletaleocwsv2-accordion.oracletaleocwsv2-accordion-expandable.clearfix")
 
     data = []
 
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "td").text.strip(),
-                com,
-                item.find_elements(By.CSS_SELECTOR, "td")[1].text.strip(),
-                link,
-            ]
-        )
+        location = item.find_elements(By.CSS_SELECTOR, 'div[tabindex="0"]')[1].text.strip()
+        for str in locations:
+            if (str in location):   
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
     driver.quit()
     
