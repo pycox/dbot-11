606e28a51366748a6f1970e8a5e9afd602557846
Shiva <shivanand.gupta@outlook.com>
2024-07-26 13:17:42 +0530

    Update the scraper 101

--- scripts/scraper101.py
+++ scripts/scraper101.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 101
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -23,6 +21,14 @@ def main():
         print(f"Scraper{key} cookie Button: {e}")
 
     time.sleep(4)
+    
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            driver.find_element(By.CSS_SELECTOR, "button.button__button--2de5X.button__normal--14TuV.secondary__default--2ySVn").click()
+        except Exception:
+            flag = False
 
     items = driver.find_elements(By.CSS_SELECTOR, 'li[data-ui="job-item"]')
 
@@ -34,15 +40,17 @@ def main():
             By.CSS_SELECTOR, 'span[data-ui="job-card-location"]'
         ).text.strip()
 
-        if location.split(",")[-1].strip() in ["United Kingdom", "United States"]:
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
     driver.quit()
 
