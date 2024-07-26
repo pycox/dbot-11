b65c2e1fc0bd903544b5354828a5f45f1cc69bdd
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:56:24 +0530

    Update the scraper 144

--- scripts/scraper144.py
+++ scripts/scraper144.py
@@ -5,60 +5,38 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 144
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    try:
-        time.sleep(4)
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='distanceLocation']").click()
-        time.sleep(2)
-        driver.find_element(By.XPATH, "//label[contains(text(), 'London')]").click()
-        time.sleep(4)
-        driver.find_element(By.XPATH, "//label[contains(text(), 'Fort Lauderdale')]").click()
-        time.sleep(4)
-        driver.find_element(By.XPATH, "//label[contains(text(), 'New York')]").click()
-        time.sleep(4)
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='viewAllJobsButton']").click()
-        time.sleep(4)
-        
-        flag = True
-        data = []
-        
-        while flag:
-            try:
-                time.sleep(4)
-                
-                items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
-                
-                for item in items:
-                    link = item.find_element(By.CSS_SELECTOR, "a").get_attribute('href')
-                                    
-                    data.append([
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+    
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".job-post")
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        location = item.find_element(By.CSS_SELECTOR, ".office").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".name").text.strip(),
                         com,
-                        item.find_element(By.CSS_SELECTOR, "dd").text.strip(),
-                        link
-                    ])
-                        
-                if len(driver.find_elements(By.CSS_SELECTOR, "button[data-uxi-element-id='next']")) > 0: 
-                    driver.find_element(By.CSS_SELECTOR, "button[data-uxi-element-id='next']").click()
-                else: 
-                    flag = False
-                    break
-                
-            except Exception as e:
-                flag = False
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-        updateDB(key, data)
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
+    driver.quit()
 
+    updateDB(key, data)
 
 
 if __name__ == "__main__":
     main()
-    
