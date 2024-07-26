e60ab5af961910e8e6c48261732981140719cb51
Shiva <shivanand.gupta@outlook.com>
2024-07-26 13:17:51 +0530

    Update the scraper 103

--- scripts/scraper103.py
+++ scripts/scraper103.py
@@ -5,9 +5,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 103
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -22,21 +20,22 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "li.job")
 
     data = []
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.job")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
