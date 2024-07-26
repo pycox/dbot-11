576850e6b8d2580f54ba105ecc643d2bfcb2ffa7
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:54:31 +0530

    Update the scraper 121

--- scripts/scraper121.py
+++ scripts/scraper121.py
@@ -5,33 +5,33 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 121
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
 
-    # items = driver.find_element(By.CSS_SELECTOR, "ul").find_elements(
-    #     By.CSS_SELECTOR, "li"
-    # )
+    items = driver.find_elements(
+        By.CSS_SELECTOR, "ul li.list-item"
+    )
 
     data = []
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-
-    #     data.append(
-    #         [
-    #             item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-    #             com,
-    #             "UK",
-    #             link,
-    #         ]
-    #     )
+    if "UK" in locations:
+        for item in items:
+            data.append(
+                [
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "h2")).strip(),
+                    com,
+                    "UK",
+                    url,
+                ]
+            )
 
     driver.quit()
     
