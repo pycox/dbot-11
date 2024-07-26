69a32939c467c72454bbb0ddaf11e9948684be9f
Shiva <shivanand.gupta@outlook.com>
2024-07-26 12:40:02 +0530

    Update the scraper 32

--- scripts/scraper32.py
+++ scripts/scraper32.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
@@ -15,33 +14,22 @@ def main(key, com, url, locations):
     time.sleep(4)
 
     data = []
-    
-    # flag = True
-    # while flag:
-    #     time.sleep(4)
-    #     try:
-    #         driver.find_element(By.CSS_SELECTOR, "div.d-flex.mt-4.ng-star-inserted > button").click()
-    #     except Exception:
-    #         flag = False
-    
-    # items = driver.find_elements(By.CSS_SELECTOR, "li.careers-listings-item")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, ".careers-listings-item--location").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "div[data-id=\"8bbddac\"] p strong")
+        for item in items:
+            title = item.text.split(":")[-1].strip()
+            data.append(
+                [
+                    title,
+                    com,
+                    "United Kingdom",
+                    url,
+                ]
+            )
 
     driver.quit()
+
     updateDB(key, data)
 
 
