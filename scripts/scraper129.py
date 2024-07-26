a64fbf743f8c14401cb93c2aeb2dc1e1459772a5
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:55:08 +0530

    Update the scraper 129

--- scripts/scraper129.py
+++ scripts/scraper129.py
@@ -5,47 +5,39 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 129
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(2)
+    time.sleep(4)
 
-    try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
-
-    time.sleep(2)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+    items = driver.find_elements(By.CSS_SELECTOR, ".content-panel-container .content-panel.contact-block")
 
     data = []
+    regions = []
+
+    if "US" in locations:
+        regions.extend(("New York", "Los Angeles"))
+    if "UK" in locations:
+        regions.append("London")
 
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        title = item.find_element(
-            By.CSS_SELECTOR, "div.job-vacancy__title"
-        ).text.strip()
-        location = item.find_elements(By.CSS_SELECTOR, "div.job-vacancy__table > div")[
-            2
-        ].text.strip()
-
-        data.append(
-            [
-                title,
-                com,
-                location,
-                link,
-            ]
-        )
+        location = item.find_element(By.CSS_SELECTOR, '.contact-header-pnl p').text.strip()
+        if location not in regions:
+            continue
+        sub_items = item.find_elements(By.CSS_SELECTOR, ".job-listings-block a")
+        for sub_item in sub_items:
+            data.append(
+                [
+                    sub_item.text.strip(),
+                    com,
+                    location,
+                    sub_item.get_attribute("href").strip(),
+                ]
+            )
 
     driver.quit()
 
