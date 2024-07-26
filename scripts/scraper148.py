fab4f0698771fc3cb67ece9b790721246df8b8b1
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:56:45 +0530

    Update the scraper 148

--- scripts/scraper148.py
+++ scripts/scraper148.py
@@ -1,24 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 148
-    com, url = readUrl(key)
+
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
+    time.sleep(4)
 
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'a#CybotCookiebotDialogBodyButtonDecline').click()
+    except:
+        print("No Cookie Button")
+        
+    time.sleep(4)
 
+    items = driver.find_elements(By.CSS_SELECTOR, ".JobListRow.details")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, '.subColumn-right').text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
 
 
 if __name__ == "__main__":
     main()
-    
