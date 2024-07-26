27f651b60c005c772c32301f116f04a2de4ec510
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:56:19 +0530

    Update the scraper 143

--- scripts/scraper143.py
+++ scripts/scraper143.py
@@ -5,17 +5,34 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 143
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-  
+    time.sleep(4)
 
-    # updateDB(key, data)
+    items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+
+    data = []
+
+    if "UK" in locations:
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "div.job-vacancy__apply > a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "div.job-vacancy__title").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+    driver.quit()
+
+    updateDB(key, data)
 
 
 if __name__ == "__main__":
