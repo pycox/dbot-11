03e62e95c21c13911542fed3f662af316395a661
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:54:44 +0530

    Update the scraper 124

--- scripts/scraper124.py
+++ scripts/scraper124.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from utils import readUrl, updateDB
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+def main(key, com, url, locations):
 
-def main():
-    key = 124
-    com, url = readUrl(key)
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,37 +15,39 @@ def main():
 
     time.sleep(4)
 
+    iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "frame[title=\"Site content frame\"]")))
+    driver.switch_to.frame(iframe)
+
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    time.sleep(2)
+    time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "li.BambooHR-ATS-Jobs-Item")
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            button = driver.find_element(By.CSS_SELECTOR, "button.Mhr-jobSearchMoreResultsButton")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            button.click()
+        except Exception as e:
+            print(e)
+            flag = False
+
 
     data = []
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(
-    #         By.CSS_SELECTOR, "span.BambooHR-ATS-Location"
-    #     ).text.strip()
-
-    #     for str in [
-    #         "London",
-    #         "New York",
-    #         "San Francisco",
-    #         "United States",
-    #         "United Kingdom",
-    #     ]:
-    #         if str in location:
-
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".Mhr-jobSearchJobs .Mhr-jobDetail")
+        for item in items:
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a span").text.strip(),
+                    com,
+                    "UK",
+                    url,
+                ]
+            )
 
     driver.quit()
 
