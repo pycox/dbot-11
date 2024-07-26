fe7625058c487a4f769d3a5d1f34fe6d3a2368bf
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:56:28 +0530

    Update the scraper 145

--- scripts/scraper145.py
+++ scripts/scraper145.py
@@ -5,13 +5,36 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 145
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".d-flex.align-items-center.justify-content-between")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, '.location').text.strip()
+
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".job_title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
 
 
 if __name__ == "__main__":
