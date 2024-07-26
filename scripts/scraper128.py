06d0153698b70a993f98d5fbb960323b5906bf7b
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:55:02 +0530

    Update the scraper 128

--- scripts/scraper128.py
+++ scripts/scraper128.py
@@ -1,54 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 128
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
     time.sleep(4)
 
-    try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#hs-eu-confirmation-button",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
-
-    time.sleep(2)
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight - 3000);")
-
-    time.sleep(8)
-    
-    items = driver.find_elements(By.CSS_SELECTOR, "div.job")
-
     data = []
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job-item-content")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a.job-description").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "London, UK",
+                    link,
+                ]
+            )
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        title = item.find_element(By.CSS_SELECTOR, "h2").text.strip()
-        location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
-
-        data.append(
-            [
-                title,
-                com,
-                location,
-                link,
-            ]
-        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
