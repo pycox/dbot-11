ab88c24344960f5ecfb91e1d80f81d7b1e8974e4
Shiva <shivanand.gupta@outlook.com>
2024-07-26 19:54:26 +0530

    Update the scraper 120

--- scripts/scraper120.py
+++ scripts/scraper120.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 120
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -26,19 +25,20 @@ def main():
 
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, "article.featured-card")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "article.featured-card")
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
