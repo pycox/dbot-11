3b8731dd2e6558f76e9fb01db6162c226aff6486
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:13:31 +0530

    Update the scraper 162

--- scripts/scraper162.py
+++ scripts/scraper162.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 162
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -27,25 +26,21 @@ def main():
 
     data = []
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".item-content.row")
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     title = item.find_element(
-    #         By.CSS_SELECTOR, "div.job-vacancy__title"
-    #     ).text.strip()
-    #     location = item.find_elements(By.CSS_SELECTOR, "div.job-vacancy__table > div")[
-    #         2
-    #     ].text.strip()
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            title = item.find_element(By.CSS_SELECTOR, "h4").text.strip()
 
-    #     data.append(
-    #         [
-    #             title,
-    #             com,
-    #             location,
-    #             link,
-    #         ]
-    #     )
+            data.append(
+                [
+                    title,
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
