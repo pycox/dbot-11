64b38478cef2e7a8fde47a6a01dded89ef1882db
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:14:33 +0530

    Update the scraper 175

--- scripts/scraper175.py
+++ scripts/scraper175.py
@@ -5,35 +5,42 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 175
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    time.sleep(8)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "li.ptcom-design__listItem__1pha1h")
+
+    items = driver.find_elements(By.CSS_SELECTOR, "#open-positions ul li ul li ul li")
 
     data = []
 
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a.ptcom-design__sublistLink__1pha1h").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'a.ptcom-design__sublistLink__1pha1h > span:last-child').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "button").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+        button = item.find_elements(By.CSS_SELECTOR, "button")
+        if not button:
+            continue
+        button = button[0]
+        driver.execute_script("arguments[0].click();", button)
+        title = item.find_element(By.CSS_SELECTOR, "button").text.split("\n")[0].strip()
+        locs = item.find_elements(By.CSS_SELECTOR, 'a')
+        for location in locs:
+            link = location.get_attribute("href").strip()
+            location = driver.execute_script("return arguments[0].innerText;", location).replace("↳", "").strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            title,
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
     driver.quit()
 
