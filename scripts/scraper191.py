0575eda4c5ae82317eed25073b1cc6c80b54d221
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:16:02 +0530

    Update the scraper 191

--- scripts/scraper191.py
+++ scripts/scraper191.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 191
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,19 +14,21 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
 
     data = []
 
+    items = driver.find_elements(By.CSS_SELECTOR, "#BambooHR-ATS li.BambooHR-ATS-Jobs-Item")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.mt-1.text-md > span:last-child').text.strip()
+        location = item.find_element(By.CSS_SELECTOR, '.BambooHR-ATS-Location').text.strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "span").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
                         com,
                         location,
                         link,
