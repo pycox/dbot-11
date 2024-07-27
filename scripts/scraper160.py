b3ba31c19478d46dfdc9a966b165bd938f901c54
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:13:21 +0530

    Update the scraper 160

--- scripts/scraper160.py
+++ scripts/scraper160.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 160
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -24,28 +23,26 @@ def main():
         print(f"Scraper{key} cookiee button: {e}")
 
     time.sleep(2)
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, "#job-listings li"))
+    time.sleep(4)
 
     data = []
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "#job-listings li a")
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     title = item.find_element(
-    #         By.CSS_SELECTOR, "div.job-vacancy__title"
-    #     ).text.strip()
-    #     location = item.find_elements(By.CSS_SELECTOR, "div.job-vacancy__table > div")[
-    #         2
-    #     ].text.strip()
-
-    #     data.append(
-    #         [
-    #             title,
-    #             com,
-    #             location,
-    #             link,
-    #         ]
-    #     )
+        for item in items:
+            link = item.get_attribute("href").strip()
+            title = item.text.strip()
+            title = driver.execute_script("return arguments[0].innerText;", item)
+            data.append(
+                [
+                    title.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
