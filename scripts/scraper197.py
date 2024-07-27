b6d9dffb2f343e3f11260367428a11a50660ca70
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:16:29 +0530

    Update the scraper 197

--- scripts/scraper197.py
+++ scripts/scraper197.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 197
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -21,9 +20,9 @@ def main():
 
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "div > a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'p.jss-f71').text.strip()
+        location = item.find_elements(By.CSS_SELECTOR, 'p')[1].text.strip()
         
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
