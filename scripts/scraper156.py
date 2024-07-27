547200fdfbb08a22467f0ba30fd041cce110f76a
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:12:56 +0530

    Update the scraper 156

--- scripts/scraper156.py
+++ scripts/scraper156.py
@@ -5,65 +5,34 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 156
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#onetrust-accept-btn-handler",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookie Button: {e}")
-
-    time.sleep(4)
-
-    flag = True
     data = []
 
-    # while flag:
-    #     try:
-    #         time.sleep(4)
+    time.sleep(4)
 
-    #         items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+    time.sleep(4)
+    items = driver.find_elements(By.CSS_SELECTOR, ".open-position-listing.w-dyn-item")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, '.open-position-item-wrapper:nth-child(3)').text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".open-position-title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    #         for item in items:
-    #             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-    #             location = item.find_element(By.CSS_SELECTOR, "dd").text.strip()
-
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-
-    #         if (
-    #             len(
-    #                 driver.find_elements(
-    #                     By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
-    #                 )
-    #             )
-    #             > 0
-    #         ):
-    #             driver.find_element(
-    #                 By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
-    #             ).click()
-    #         else:
-    #             flag = False
-    #             break
-
-        # except Exception as e:
-        #     flag = False
 
     driver.quit()
 
