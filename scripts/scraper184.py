71816e04b0d1ab97871a0c19f6b7eab9dbb90d79
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:15:22 +0530

    Update the scraper 184

--- scripts/scraper184.py
+++ scripts/scraper184.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 184
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,19 +14,19 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.career-card")
+    items = driver.find_elements(By.CSS_SELECTOR, ".row.vacancy__render")
 
     data = []
 
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.career-details > p').text.strip()
+        location = item.find_element(By.CSS_SELECTOR, 'span[title="Location"]').text.strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
                         com,
                         location,
                         link,
