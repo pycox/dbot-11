1771e4afa6e627e172de4e785e1b119a4d4e7250
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:15:55 +0530

    Update the scraper 190

--- scripts/scraper190.py
+++ scripts/scraper190.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 190
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,25 +14,30 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
 
     data = []
 
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            button = driver.find_element(By.CSS_SELECTOR, "#show_more_button")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+        except Exception:
+            flag = False
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.mt-1.text-md > span:last-child').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "span").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+        data.append(
+            [
+                driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "span")).strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
 
     driver.quit()
 
