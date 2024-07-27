ca850e7f69ef9f0ff7cc824f96020d8c90284aa1
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:16:47 +0530

    Update the scraper 200

--- scripts/scraper200.py
+++ scripts/scraper200.py
@@ -5,39 +5,44 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 200
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    driver.execute_script("window.scrollBy(0, 10000);")
-
-    time.sleep(4)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "article.single-job-opportunity")
 
     data = []
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div > p > span.single-job-opportunity__result').text.strip()
+    if "UK" in locations:
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
+        flag = True
+        while flag:
+            driver.execute_script("window.scrollBy(0, 10000);")
+            time.sleep(4)
+            
+            items = driver.find_elements(By.CSS_SELECTOR, "#vacancies article.single-job-opportunity")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "h4")).strip(),
                         com,
-                        location,
+                        "UK",
                         link,
                     ]
                 )
-                break
+                
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, 'a.next')
+                driver.execute_script("arguments[0].scrollIntoView();", button)
+                time.sleep(2)
+                driver.execute_script("arguments[0].click();", button)
+            except Exception as e:
+                flag = False
+                print("No More Jobs", e)
 
     driver.quit()
-
     updateDB(key, data)
 
 
