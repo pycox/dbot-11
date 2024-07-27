04fbbfe23a1d14379f80316e3d862f9ccb5573a0
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:13:12 +0530

    Update the scraper 159

--- scripts/scraper159.py
+++ scripts/scraper159.py
@@ -5,61 +5,29 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 159
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    # time.sleep(4)
-
-    # try:
-    #     driver.find_element(By.CSS_SELECTOR, "a#hs-eu-confirmation-button").click()
-    # except Exception as e:
-    #     print(f"Scraper{key} cookiee button: {e}")
-
     time.sleep(4)
 
     data = []
-    flag = True
 
-    while flag:
-        try:
-            time.sleep(4)
-
-            driver.find_element(By.CSS_SELECTOR, "button[rel='next']").click()
-
-        except Exception as e:
-            flag = False
-            break
-
-    items = driver.find_elements(By.CSS_SELECTOR, "li.whr-item")
+    items = driver.find_elements(By.CSS_SELECTOR, "#lrz5zyjl a")
 
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        title = item.find_element(By.CSS_SELECTOR, "h3.whr-title").text.strip()
-        locations = item.find_elements(By.CSS_SELECTOR, "li.whr-location")
-
-        if len(locations) > 0:
-            if locations[0].text.strip() in [
-                "London",
-                "Manchester",
-                "Manchester",
-                "Birmingham",
-                "Leeds",
-                "Reading",
-                "Maidenhead",
-            ]:
-                data.append(
-                    [
-                        title,
-                        com,
-                        locations[0].text.strip(),
-                        link,
-                    ]
-                )
+        link = item.get_attribute("href")
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                com,
+                locations[0],
+                link,
+            ]
+        )
 
     driver.quit()
 
