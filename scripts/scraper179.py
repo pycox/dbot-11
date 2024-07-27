04523ca22ae24a8fd995adf5d41278b7b0f987e7
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:14:55 +0530

    Update the scraper 179

--- scripts/scraper179.py
+++ scripts/scraper179.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 179
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -24,7 +23,7 @@ def main():
         location = item.find_element(By.CSS_SELECTOR, 'div.career-details > p').text.strip()
         location = location.replace("Location:", "").strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
