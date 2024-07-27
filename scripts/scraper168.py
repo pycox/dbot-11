c08d3986ddfa6761e90153dcc3ca1c3ddb78f14e
Shiva <shivanand.gupta@outlook.com>
2024-07-27 13:13:58 +0530

    Update the scraper 168

--- scripts/scraper168.py
+++ scripts/scraper168.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 168
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,36 +16,47 @@ def main():
     time.sleep(4)
 
     try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#onetrust-accept-btn-handler",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
+        driver.find_element(By.CSS_SELECTOR, 'button#accept-cookies').click()
+    except:
+        print("No Cookie Button")
 
     time.sleep(4)
 
     data = []
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".items-stretch.ng-star-inserted a.ng-star-inserted")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, "p.paragraph-2.font-medium.text-neutral-600").text.strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.card-body")
+        try:
+            
+            next_button = driver.find_element(By.CSS_SELECTOR, '#next-page')
+            if next_button.get_attribute("disabled"):
+                flag = False
+            else:
+                next_button.click()
+                
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        location = item.find_element(
-            By.XPATH, "//div[small[text()='Location']]/following-sibling::div"
-        ).text.strip()
-
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
