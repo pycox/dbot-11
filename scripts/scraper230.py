ebcff2a570eb55c165e26a118e06a0aa9a0f8580
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:24:36 +0530

    Update the scraper 230

--- scripts/scraper230.py
+++ scripts/scraper230.py
@@ -1,39 +1,77 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 230
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
-
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+    except:
+        print("No Cookie Button")
     data = []
+    regions = []
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+    if "UK" in locations:
+        regions.append(("United Kingdom", "29247e57dbaf46fb855b224e03170bc7"))
 
-    #     data.append(
-    #         [
-    #             item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #             com,
-    #             location,
-    #             link,
-    #         ]
-    #     )
+    if "US" in locations:
+        regions.append(("United States", "bc33aa3152ec42d4995f4791a106ed09"))
+        
+    for location, location_id in regions:
+        try:
+            driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='distanceLocation']").click()
+            time.sleep(3)
+            driver.find_element(By.CSS_SELECTOR, "label[for='location']").click()
+            time.sleep(3)
+            driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, f"label[for='{location_id}']"))
+            driver.find_element(By.CSS_SELECTOR, "div[data-automation-id='sidebar'").click()
+            
+            time.sleep(3)
+        except:
+            continue
+
+        flag = True
+
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+
+            
+            try:
+                driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, 'button[data-uxi-element-id="next"]'))
+                
+                time.sleep(4)
+            except Exception:
+                flag = False
+                print("No More Jobs")
+        
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'button[data-automation-id="clearAllButton"]').click()
+            time.sleep(4)
+        except:
+            print("No filter to clear")
 
     driver.quit()
-
     updateDB(key, data)
 
 
