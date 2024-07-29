e49cb0c521eaf64411c19bb6c1bd194cb871eba4
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:25:38 +0530

    Update the scraper 243

--- scripts/scraper243.py
+++ scripts/scraper243.py
@@ -1,43 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 243
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    count = 0
-    while count < 3:
-      try:
-        driver.find_element(By.CSS_SELECTOR, 'a.filters-more').click()
-        time.sleep(4)
-        count += 1
-      except:
-        count = 3
-        print("No more Jobs")
-
-    items = driver.find_elements(By.CSS_SELECTOR, "a.table-tr.filter-box.tag-active.joblink")
+    time.sleep(6)
 
     data = []
-
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".job_collection_item.data-id")
     for item in items:
-        link = item.get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div:nth-child(2) > div').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".job_pagelist_meta.list_js_office").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "div > div").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
                         com,
                         location,
                         link,
@@ -45,8 +35,8 @@ def main():
                 )
                 break
 
-    driver.quit()
 
+    driver.quit()
     updateDB(key, data)
 
 
