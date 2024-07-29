dfbeeef12c5a46bb10bda6d05b01ea0d11e633ca
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:25:57 +0530

    Update the scraper 247

--- scripts/scraper247.py
+++ scripts/scraper247.py
@@ -1,48 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 247
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    flag = True
+    time.sleep(2)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
+    except Exception as e:
+        print(f"Scraper{key} cookiee button: {e}")
+
+    time.sleep(2)
     data = []
+    regions = []
 
-    while flag:
+    if "UK" in locations:
+        regions.append("United Kingdom")
+    if "US" in locations:
+        regions.append("United States")
+    
+    for region in regions:
+        Select(driver.find_element(By.CSS_SELECTOR, "select#careers_filter_dropdown")).select_by_value(region)
+        driver.find_element(By.CSS_SELECTOR, "input[value=\"Update results\"]").click()
         time.sleep(4)
-        items = driver.find_elements(By.CSS_SELECTOR, "div.page-center.w-full.featured-positions.flex.flex-col.gap-y-4 > div")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, 'div').text.strip()
+        
+        flag = True
+        
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, ".careers-search .careers-featured-cards")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                title = driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "h3"))
+                
+                data.append(
+                    [
+                        title.strip(),
+                        com,
+                        region,
+                        link,
+                    ]
+                )
 
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
-        try:
-          driver.find_element(By.CSS_SELECTOR, "a[text='Next >']").click()
-          time.sleep(4)
-        except:
-          flag = False
-          print("No More Jobs")
-
-
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, 'a.px-4.py-2.border.bg-white.leading-2')
+                driver.execute_script("arguments[0].click();", next_button)
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
+        
     driver.quit()
 
     updateDB(key, data)
