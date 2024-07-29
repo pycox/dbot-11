2defec3ac0af853635f59eee29044041aa0fea01
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:24:40 +0530

    Update the scraper 231

--- scripts/scraper231.py
+++ scripts/scraper231.py
@@ -1,40 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 231
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button[mode="primary"]').click()
+    except:
+        print("No Cookie Button")
+    time.sleep(4)
 
     data = []
 
+    items = driver.find_elements(By.CSS_SELECTOR, "#tab-annunci .col-12.col-md-6")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "div.job-vacancy__apply > a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.job-vacancy__table > div:nth-child(3)').text.strip()
-        location = location.replace("Location:", "").strip()
+        link = item.find_element(By.CSS_SELECTOR, ".vacancy__title a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "span[title='Location']").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".vacancy__title a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "div.job-vacancy__title").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
