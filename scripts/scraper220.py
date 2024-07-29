b36a64ea5e22903710ed9080f8629a3e5a2fb0ac
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:23:47 +0530

    Update the scraper 220

--- scripts/scraper220.py
+++ scripts/scraper220.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 220
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -18,27 +17,27 @@ def main():
     flag = True
     while flag:
       try:
-        button = driver.find_element(By.CSS_SELECTOR, "div.flex.justify-center > button").click()
+        driver.find_element(By.CSS_SELECTOR, "div.flex.justify-center > button").click()
         time.sleep(4)
       except:
         flag = False
         print("No more load button")
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.comp-listing-vacancy_vacancy_item__RGJz4.vacancy-item")
 
     data = []
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.comp-listing-vacancy_meta_data__Ks_yc.ss-sp-30 > span').text.strip()
-
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "div.fs-title-4.text-greenDark.ss-sp-40").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "div.comp-listing-vacancy_vacancy_item__RGJz4.vacancy-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "div.fs-title-4.text-greenDark.ss-sp-40").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
