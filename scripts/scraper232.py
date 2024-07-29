e23bd9cf3a94354f19ed5c6eadb86e9d6328db23
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:24:44 +0530

    Update the scraper 232

--- scripts/scraper232.py
+++ scripts/scraper232.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 232
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -29,9 +28,9 @@ def main():
 
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'span[data-ui="job-location"]').text.strip()
+        location = item.find_element(By.CSS_SELECTOR, 'div[data-ui="job-location"]').text.strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
