ee2a682a978b619b08c2f7d7cbb26ca39a16d788
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:24:53 +0530

    Update the scraper 234

--- scripts/scraper234.py
+++ scripts/scraper234.py
@@ -1,32 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 234
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(8)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "div.grid-container.feed")
+    time.sleep(4)
 
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".col-sm-12 .result-wrapper")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.feed-item-location').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str.lower() in location.lower()):
+        link = item.find_element(By.CSS_SELECTOR, "h3.result-title a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".result-info").text.split("|")[-1].strip()
+        for str in locations:
+            if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h3.result-title a").text.strip(),
                         com,
                         location,
                         link,
@@ -34,8 +33,8 @@ def main():
                 )
                 break
 
-    driver.quit()
 
+    driver.quit()
     updateDB(key, data)
 
 
