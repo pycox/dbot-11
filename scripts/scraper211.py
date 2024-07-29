7d6f5c35dba7a96b48a8e623d91b9809655a9921
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:23:01 +0530

    Update the scraper 211

--- scripts/scraper211.py
+++ scripts/scraper211.py
@@ -5,45 +5,51 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 211
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
-    driver.get(url)
 
-    time.sleep(4)
     data = []
 
-    total_jobs = driver.find_element(By.CSS_SELECTOR, 'span.paginationLabel').text.strip()
-    total_jobs = total_jobs.split("of")[1].strip()
-    
-    current_jobs = 25
-    while current_jobs < int(total_jobs):
-      items = driver.find_elements(By.CSS_SELECTOR, "tr.data-row")
-      
-      for item in items:
-          link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-          location = item.find_element(By.CSS_SELECTOR, 'span.jobLocation').text.strip()
+    regions = {}
+    locations = ["UK", "US"]
 
-          for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US', 'GB']:
-              if (str in location):
-                  data.append(
-                      [
-                          item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                          com,
-                          location,
-                          link,
-                      ]
-                  )
-                  break
-        
-      current_jobs += 25
-      if current_jobs < int(total_jobs):
-        driver.get(url.split("?")[0] + "?q=&sortColumn=referencedate&sortDirection=desc&startrow="+str(current_jobs))
+    if "UK" in locations:
+        regions["GB"] = "United Kingdom"
+
+    if "US" in locations:
+        regions["US"] = "United States"
+
+
+    for region_code, region in regions.items():
+        url_ = f"{url}?optionsFacetsDD_country={region_code}"
+        driver.get(url_)
         time.sleep(4)
 
+        total_jobs = driver.find_element(By.CSS_SELECTOR, 'span.paginationLabel').text.strip()
+        total_jobs = int(total_jobs.split("of")[1].strip())
+
+        current_jobs = 0
+        while current_jobs < total_jobs:
+            items = driver.find_elements(By.CSS_SELECTOR, "tr.data-row")
+
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        region,
+                        link,
+                    ]
+                )
+
+            current_jobs += 25
+            if current_jobs < total_jobs:
+                driver.get(url_ + f"&startrow={current_jobs}")
+                time.sleep(4)
 
     driver.quit()
 
