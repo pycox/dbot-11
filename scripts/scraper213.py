e9e470cba8b092aece56db615e4b27784aa5a970
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:23:12 +0530

    Update the scraper 213

--- scripts/scraper213.py
+++ scripts/scraper213.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 213
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -22,21 +21,21 @@ def main():
         
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.x-callout.left-text")
-
     data = []
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "div.x-callout.left-text")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "h2.h-callout").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2.h-callout").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
