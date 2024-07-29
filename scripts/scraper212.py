a1b85126c67f567cc1cbdf7b1570426809548135
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:23:06 +0530

    Update the scraper 212

--- scripts/scraper212.py
+++ scripts/scraper212.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 212
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,24 +14,25 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "strong.font-serif.text-xl.my-7.block")
 
     data = []
 
-    for item in items:
-        try:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "strong.font-serif.text-xl.my-7.block")
+        for item in items:
+            try:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                    com,
-                    "UK",
-                    link,
-                ]
-            )
-        except:
-          print("No Job")
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        "UK",
+                        link,
+                    ]
+                )
+            except:
+                print("No Job")
 
     driver.quit()
 
