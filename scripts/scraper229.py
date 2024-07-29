6a897d4832170b4f8ee87befa3e9fcb53f09bd4a
Shiva <shivanand.gupta@outlook.com>
2024-07-29 14:24:28 +0530

    Update the scraper 229

--- scripts/scraper229.py
+++ scripts/scraper229.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 229
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,20 +14,21 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.css-t3gd2t > h3")
-
     data = []
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "div.css-t3gd2t > h3")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
