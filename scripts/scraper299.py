596615c7c61117475b6bf04854950fc4a75cfb20
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:39:42 +0530

    Update the scraper 299

--- scripts/scraper299.py
+++ scripts/scraper299.py
@@ -6,45 +6,42 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 299
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    try:
-        driver.find_element(By.CSS_SELECTOR, "button#hs-eu-decline-button").click()
-    except:
-        print("No Cookie Button")
-
-    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, ".jobsList"))
-    
-    time.sleep(4)
+    time.sleep(2)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, "div.jobs-list .job")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "p.tags").text.strip()
-        for str in [
-            'Reading', 'London', 'New York', 'San Francisco', 'United States',
-            'United Kingdom', 'UK', 'USA', 'US'
-        ]:
-            if (str in location):
+    if "UK" in locations:
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, ".cmp-teaser.card")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "a h2.job-title").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
                         com,
-                        location,
+                        "United Kingdom",
                         link,
                     ]
                 )
-                break
+
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, '.cmp-pagination__link-next')
+                if "disabled" in next_button.get_attribute("class"):
+                    flag = False
+                else:
+                    driver.execute_script("arguments[0].click();", next_button)
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
 
     driver.quit()
     updateDB(key, data)
