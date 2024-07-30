f98f500fed45a1bd0c9af9dd06459f9b96932f87
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:52 +0530

    Update the scraper 260

--- scripts/scraper260.py
+++ scripts/scraper260.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 260
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,29 +15,22 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "a.job")
-
     data = []
-
-    for item in items:
-        link = item.get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.roles_text').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US', 'GB']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".positions li.position")
+        for item in items:
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "UK",
+                    item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip(),
+                ]
+            )
 
 
     driver.quit()
-
     updateDB(key, data)
 
 
