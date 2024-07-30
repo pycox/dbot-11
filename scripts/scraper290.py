d78b008315b2da86360758a32b2d8aac9c78cadc
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:39:04 +0530

    Update the scraper 290

--- scripts/scraper290.py
+++ scripts/scraper290.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 290
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,28 +15,24 @@ def main():
 
     time.sleep(4)
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     data = []
-
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
-
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".et_pb_toggle.et_pb_toggle_item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    com,
+                    "United Kingdom",
+                    link,
+                ]
+            )
 
     driver.quit()
-
     updateDB(key, data)
 
 
