40923d122cd3349af991967a34d8a3952d1c0e19
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:37:20 +0530

    Update the scraper 267

--- scripts/scraper267.py
+++ scripts/scraper267.py
@@ -1,42 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 267
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
+    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
-
+    
     data = []
+        
+    flag = True
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+        for item in items:
+            location = item.find_element(By.CSS_SELECTOR, "div[data-automation-id=\"locations\"] dd").text.strip()
+            for str in locations:
+                if (str in location):
+                    link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
-
+        
+        try:
+            driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, 'button[data-uxi-element-id="next"]'))
+            time.sleep(4)
+        except Exception:
+            flag = False
+            print("No More Jobs")
+    
     driver.quit()
-
     updateDB(key, data)
 
 
