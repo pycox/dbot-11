f26f6ff1823af73a7af9c021fbd05eddd1043b4e
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:38:30 +0530

    Update the scraper 282

--- scripts/scraper282.py
+++ scripts/scraper282.py
@@ -1,42 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 282
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
-    # items = driver.find_elements(By.CSS_SELECTOR, "div#positions")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
 
     data = []
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".jobs-list app-job-card")
+        if not items:
+            flag = False
+            continue
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".job-card-location").text.strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'button.o-buttons-icon--arrow-right.o-buttons-icon--icon-only').click()
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
     driver.quit()
-
     updateDB(key, data)
 
 
