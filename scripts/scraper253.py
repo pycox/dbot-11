df497589445fd8ce598e809fbe5667f08237317d
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:20 +0530

    Update the scraper 253

--- scripts/scraper253.py
+++ scripts/scraper253.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 253
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,25 +14,25 @@ def main():
 
     time.sleep(4)
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
-
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "tr.job-post")
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, '.body.body__secondary.body--metadata').text.strip()
 
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".body.body--medium").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
     driver.quit()
 
