dbb53adb8b3e562ab0bb67b6406689b148d37bdf
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:38:01 +0530

    Update the scraper 276

--- scripts/scraper276.py
+++ scripts/scraper276.py
@@ -5,9 +5,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 276
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,15 +14,15 @@ def main():
 
     time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "ul.b--careers-list-a > li")
 
     data = []
 
+    items = driver.find_elements(By.CSS_SELECTOR, "ul.b--careers-list-a > li")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, 'span.b--careers-list-a__list-item__link__item-right').text.strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
