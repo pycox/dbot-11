6a97be5eef891581cb4e595aa40106f5ea03f285
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:37:44 +0530

    Update the scraper 272

--- scripts/scraper272.py
+++ scripts/scraper272.py
@@ -5,31 +5,53 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 272
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-    items = driver.find_elements(By.CSS_SELECTOR, "div#ctl00_ctl00_ContentContainer_mainContent_ctl00_jobResultListNew > div.vsr-job")
+    time.sleep(3)
+
+    try:
+        iframe = driver.find_element(By.CSS_SELECTOR, "iframe[title=\"SP Consent Message\"]")
+        driver.switch_to.frame(iframe)
+        button = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="I Agree"]')
+        driver.execute_script("arguments[0].click();", button)
+        driver.switch_to.default_content()
+        time.sleep(2)
+    except:
+        print("No Cookie Button")
+
 
     data = []
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div[data-id="div_content_VacV_LocationID"] > span').text.strip()
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, "div#ctl00_ctl00_ContentContainer_mainContent_ctl00_jobResultListNew > div.vsr-job")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, 'div[data-id="div_content_VacV_LocationID"] > span').text.strip()
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    location,
+                    link,
+                ]
+            )
+        
+        try:
+            curr_button = int(driver.find_element(By.CSS_SELECTOR, '.paginator.paginator--bottom .cpb').text.strip())
+            next_button = driver.find_element(By.CSS_SELECTOR, f'.paginator.paginator--bottom a[title="Go to page {curr_button+1}"]')
+            driver.execute_script("arguments[0].scrollIntoView();", next_button)
+            driver.execute_script("arguments[0].click();", next_button)
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
         
     driver.quit()
 
