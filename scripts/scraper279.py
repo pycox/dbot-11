5e19890d43f6b17e73723daf3338bdfb747ed937
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:38:15 +0530

    Update the scraper 279

--- scripts/scraper279.py
+++ scripts/scraper279.py
@@ -5,35 +5,54 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 279
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "div.ListGridContainer > div")
-
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button.cc-nb-okagree').click()
+        time.sleep(4)
+    except:
+        print("No Cookiee") 
+        
     data = []
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job-search-section__result a.job-card")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".job-card__list li:nth-child(1)").text.strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'span.codelist5value_vacancyColumn.vacancyColumn').text.strip()
+        try:
+            next_button = driver.find_element(By.CSS_SELECTOR, '.pagination__btn--next')
+            if "is-disabled" in next_button.get_attribute("class"):
+                flag = False
+            else:
+                driver.execute_script("arguments[0].click();", next_button)
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
