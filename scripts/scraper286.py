298a128a3b86ddf3f2332db4a06159ab89501e43
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:38:46 +0530

    Update the scraper 286

--- scripts/scraper286.py
+++ scripts/scraper286.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 286
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,37 +15,27 @@ def main():
 
     time.sleep(4)
 
-    try:
-      button = driver.find_element(By.CSS_SELECTOR, "a.RichTextBlocklink")
-      if button:
-        driver.execute_script("arguments[0].click();", button)
-    except:
-      print("No Listing Page")
-
-    time.sleep(4)
-
-    # items = driver.find_elements(By.CSS_SELECTOR, "table.MuiTable-root-630 > tbody > tr")
-
     data = []
+    
+    if "UK" in locations:
+      items = driver.find_elements(By.CSS_SELECTOR, ".availablerole")
+      for item in items:
+          try:
+              title = driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, ".jobtitle"))
+          except:
+              continue
+          if title:
+              data.append(
+                  [
+                      title.strip(),
+                      com,
+                      "UK",
+                      item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                  ]
+              )
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'div.MuiBox-root-292 > p.MuiTypography-root-413').text.strip()
-
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
     driver.quit()
-
     updateDB(key, data)
 
 
