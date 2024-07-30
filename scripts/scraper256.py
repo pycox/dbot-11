dd8cf749a7051847a6745fa19694d411297aef60
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:31 +0530

    Update the scraper 256

--- scripts/scraper256.py
+++ scripts/scraper256.py
@@ -1,42 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 256
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
+    time.sleep(2)
 
     data = []
+    
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            driver.find_element(By.CSS_SELECTOR, "#show_more_button").click()
+        except Exception:
+            flag = False
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "#jobs_list_container li")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".mt-1.text-md").text
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "span.company-link-style").text.strip(),
+                        com,
+                        str,
+                        link,
+                    ]
+                )
+                break
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
-
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
     driver.quit()
-
     updateDB(key, data)
 
 
