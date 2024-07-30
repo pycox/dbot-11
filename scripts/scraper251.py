b8cebdaf0508581ca0a71a70537b515621eafc19
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:11 +0530

    Update the scraper 251

--- scripts/scraper251.py
+++ scripts/scraper251.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 251
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -23,22 +22,34 @@ def main():
       print("No Select working")
 
     time.sleep(4)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "table.ats_list > tbody > tr")
     data = []
 
-    for item in items:
-        link = item.get_attribute("onclick").strip().replace("window.location.href=", "").replace("'", "")
-        link = "https://ryman.ats.emea1.fourth.com" + link
-          
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "td:first-child").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, "table.ats_list > tbody > tr")
+
+        for item in items:
+            link = item.get_attribute("onclick").strip().replace("window.location.href=", "").replace("'", "")
+            link = "https://ryman.ats.emea1.fourth.com" + link
+            
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "td:first-child").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+            
+        try:
+            curr_button = int(driver.find_element(By.CSS_SELECTOR, 'li.page-item.active a').text.strip())
+            next_button = driver.find_element(By.CSS_SELECTOR, f'li a[data-page="{curr_button+1}"]')
+            next_button.click()
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
     
     driver.quit()
 
