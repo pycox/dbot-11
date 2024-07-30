852406515aaf38e832b70f6b57ca42f52f114e98
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:44 +0530

    Update the scraper 259

--- scripts/scraper259.py
+++ scripts/scraper259.py
@@ -5,39 +5,37 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 259
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     data = []
-    flag = True
 
-    while flag:
-        time.sleep(4)
-        items = driver.find_elements(By.CSS_SELECTOR, "section.apply-details__container")
-        
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, 'p > span').text.strip()
-
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
-        
-        try:
-          driver.find_element(By.CSS_SELECTOR, 'a.button.next').click()
-        except:
-          flag = False
-          print("No more Jobs")
+    if "UK" in locations:
+        flag = True
+        while flag:
+            time.sleep(4)
+            items = driver.find_elements(By.CSS_SELECTOR, "section.apply-details__container")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        "UK",
+                        link,
+                    ]
+                )
+            
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, 'a.button.next')
+                driver.execute_script("arguments[0].click();", button)
+            except:
+                flag = False
+                print("No more Jobs")
 
     driver.quit()
 
