ab22e6c1cf2aa1f9f61e440b01a133bb8f32953b
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:39:16 +0530

    Update the scraper 293

--- scripts/scraper293.py
+++ scripts/scraper293.py
@@ -4,9 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from utils import readUrl, updateDB
 
 
-def main():
-    key = 293
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -19,7 +18,7 @@ def main():
         link = item.find_element(By.CSS_SELECTOR, "a.posting-title").get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, 'a.posting-title .posting-categories .location').text.strip()
 
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
