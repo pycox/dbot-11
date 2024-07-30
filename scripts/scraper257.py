8dbef1bd793f1f28a3bae8e731ed1b420ad14ad4
Shiva <shivanand.gupta@outlook.com>
2024-07-30 11:36:35 +0530

    Update the scraper 257

--- scripts/scraper257.py
+++ scripts/scraper257.py
@@ -1,42 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 257
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
+    if url.endswith("/"):
+        url = url[:-1]
     driver.get(url)
 
     time.sleep(4)
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
-
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".open-roles .open-role")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".open-role-location").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".open-role-title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
-
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
     driver.quit()
-
     updateDB(key, data)
 
 
