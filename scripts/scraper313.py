db32b26256913fb1fa3c04fe011d86f66d09c254
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:42:46 +0530

    Update the scraper 313

--- scripts/scraper313.py
+++ scripts/scraper313.py
@@ -6,16 +6,20 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 313
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-    driver.find_element(By.CSS_SELECTOR, "a[data-ui='clear-filters']").click()
+    try:
+        button = driver.find_element(By.CSS_SELECTOR, "a[data-ui='clear-filters']")
+        driver.execute_script("arguments[0].scrollIntoView();", button)
+        driver.execute_script("arguments[0].click();", button)
+    except:
+        print("No filter to clear")
     
     data = []
     
@@ -23,15 +27,17 @@ def main():
     while flag:
         time.sleep(4)
         try:
-            driver.find_element(By.CSS_SELECTOR, "button[data-ui='load-more-button']").click()
+            button = driver.find_element(By.CSS_SELECTOR, "button[data-ui='load-more-button']")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
         except Exception:
             flag = False
     
     items = driver.find_elements(By.CSS_SELECTOR, ".styles--1Px_L ul li")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "span[data-ui='job-location']").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        location = item.find_element(By.CSS_SELECTOR, "div[data-ui='job-location']").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
