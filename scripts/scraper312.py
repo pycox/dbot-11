62b7782cc8339a01a34b0328b2f3b14cc27c1663
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:42:37 +0530

    Update the scraper 312

--- scripts/scraper312.py
+++ scripts/scraper312.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 312
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -17,22 +16,19 @@ def main():
     time.sleep(4)
 
     data = []
-    
-    # items = driver.find_elements(By.CSS_SELECTOR, "li.careers-listings-item")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, ".careers-listings-item--location").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, ".careers-listings-item--title").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+
+    if "UK" in locations:    
+        items = driver.find_elements(By.CSS_SELECTOR, "#job-list .text-expander h2 a")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
