030a4a9a4f055d45d33e55b9bdcc209431cd9702
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:45:26 +0530

    Update the scraper 341

--- scripts/scraper341.py
+++ scripts/scraper341.py
@@ -6,37 +6,41 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 341
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".content-item-type-post")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        title_location = item.find_element(By.CSS_SELECTOR, ".content-item-heading-text.heading-text").text.strip().split()
-        title, location = " ".join(title_location[:-2]), title_location[-1]
-        if location == "Kendal":
-            location = f"{location}, UK"
-        elif any(substring in location for substring in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']):
-            pass
-        else:
-            continue
-        data.append(
-            [
-                title,
-                com,
-                location,
-                link,
-            ]
-        )
+    if "UK" in locations:
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                last_height = driver.execute_script("return document.body.scrollHeight")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    flag = False
+            except:
+                flag = False
+    
+        items = driver.find_elements(By.CSS_SELECTOR, ".search-results.job-tile.job-list-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "span.job-tile__title").text.strip(),
+                    com,
+                    "United Kingdom",
+                    link,
+                ]
+            )
+
 
     driver.quit()
     updateDB(key, data)
