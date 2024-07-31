bdef66f18f8450805c73083f8650ff78bca181c2
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:45:22 +0530

    Update the scraper 340

--- scripts/scraper340.py
+++ scripts/scraper340.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 340
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -24,26 +23,17 @@ def main():
         for item in items:
             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
             location = item.find_element(By.CSS_SELECTOR, ".rowItemsInnerContainer2 span:nth-child(1)").text.strip()
-            if location in ["New York"]:
-                location = f"{location}, US"
-            elif location in [
-                "Belfast", "Birmingham", "Bournemouth", "Bristol", "Channel Islands", "Cheltenham", "Edinburgh",
-                "Exeter", "Glasgow", "Guernsey", "Jersey", "Leeds", "Liverpool", "Manchester", "Reading", "Reigate",
-                "Sheffield", 
-            ]:
-                location = f"{location}, UK"
-            elif any(substring in location for substring in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']):
-                pass
-            else:
-                continue
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
         try:
             driver.find_element(By.CSS_SELECTOR, 'a.normalanchor.ajaxable.scroller.scroller_movenext.buttonEnabled').click()
