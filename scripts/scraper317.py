fa0602e7d614f938ec0ab2dea36dfddbdb4103ca
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:43:04 +0530

    Update the scraper 317

--- scripts/scraper317.py
+++ scripts/scraper317.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 317
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -33,15 +32,17 @@ def main():
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, "h4").text.strip()
-        if location in ['Birmingham', 'Glasgow', 'Brighton']:
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                    com,
-                    f'{location}, United Kingdom',
-                    link,
-                ]
-            )
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
     driver.quit()
     updateDB(key, data)
