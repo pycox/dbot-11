f60440990f6034e8fafb0f366659dbed3f69ef18
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:43:50 +0530

    Update the scraper 326

--- scripts/scraper326.py
+++ scripts/scraper326.py
@@ -6,34 +6,29 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 326
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    
-    time.sleep(4)
-    data = []
 
-    for location in ['United States', 'United Kingdom']:
-        driver.find_element(By.CSS_SELECTOR, "#downshift-5-toggle-button").click()
-        driver.find_element(By.CSS_SELECTOR, f'#downshift-5-menu li[aria-label="{location}"]').click()
-        time.sleep(4)
-        items = driver.find_elements(By.CSS_SELECTOR, ".sc-6exb5d-3.gnPPfQ")
+    time.sleep(4)
+
+    data = []
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".cards__card.card")
         for item in items:
             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
             data.append(
                 [
-                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
                     com,
-                    location,
+                    "United Kingdom",
                     link,
                 ]
             )
-        driver.find_element(By.CSS_SELECTOR, "div.sc-1vzxbt3-9.himVqE > div.sc-1vzxbt3-3.jIcLux > div.sc-1vzxbt3-4.sc-1up9u4w-0.glrEFW > button[type='button']").click()
-        
 
     driver.quit()
     updateDB(key, data)
