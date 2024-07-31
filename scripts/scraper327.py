3130e890e68ee68db450235864342c82a582d3c4
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:43:54 +0530

    Update the scraper 327

--- scripts/scraper327.py
+++ scripts/scraper327.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 327
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -22,9 +21,8 @@ def main():
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, ".overline-title").text.strip()
-        location_ = location.lower()
-        for str in ['london', 'new York', 'san francisco', 'united states', 'united kingdom', 'uk', 'usa', 'us']:
-            if (str in location_):
+        for str in locations:
+            if (str in location):
                 data.append(
                     [
                         item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
