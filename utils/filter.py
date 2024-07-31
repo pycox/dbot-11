f9d66059ab84ce16020a11a27036f8854d39a9b7
Shiva <shivanand.gupta@outlook.com>
2024-07-31 17:26:13 +0530

    Update filter.py file

--- utils/filter.py
+++ utils/filter.py
@@ -74,7 +74,7 @@ def filterUrls():
     if ws["D1"].value != "yes":
         return urls
     
-    for row in ws.iter_rows(min_row=2, max_row=300):
+    for row in ws.iter_rows(min_row=2, max_row=350):
     # for row in ws.iter_rows(min_row=1):
 
         if row[0].value == "ID" or row[0].value is None:
