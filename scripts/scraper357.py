8c046ed20a5400cef0915565f3ae2325ea9c73bf
Shiva <shivanand.gupta@outlook.com>
2024-08-01 12:27:45 +0530

    Update the scraper 357

--- scripts/scraper357.py
+++ scripts/scraper357.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 357
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -30,19 +29,19 @@ def main():
         items = driver.find_elements(By.CSS_SELECTOR, ".attrax-list-widget__list--has-items .attrax-vacancy-tile")
         for item in items:
             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, ".attrax-vacancy-tile__option-location-valueset p").text.strip()
-            for location in ['Hammersmith', 'Knightsbridge', 'Thatcham', 'Harrods Estates', 'Heathrow - T1 London', 'Lakeside', 'City Road', 'Milton Keynes', 'Edinburgh', 'Bristol', 'Remote', 'Newcastle']:
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                        com,
-                        f"{location}, UK",
-                        link,
-                    ]
-                )
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
         try:
-            driver.find_element(By.CSS_SELECTOR, 'main#main-site-main-content div.row.dragElement.widget.container-widget.wrapper-widget.job-results__pagination-container > div > div > div.attrax-pagination__pagination > ul > li.attrax-pagination__next > a').click()
+            button = driver.find_element(By.CSS_SELECTOR, 'li.attrax-pagination__next a')
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
             time.sleep(4)
         except:
             flag = False
