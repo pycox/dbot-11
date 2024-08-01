a765cda525bf614450e89007fd496552e2e62f4e
Shiva <shivanand.gupta@outlook.com>
2024-08-01 12:28:50 +0530

    Update the scraper 370

--- scripts/scraper370.py
+++ scripts/scraper370.py
@@ -6,39 +6,27 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 370
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-
-    Select(driver.find_element(By.CSS_SELECTOR, "select#edit-field-position-type-value")).select_by_value("5")
-    
-    time.sleep(4)
-    
-    driver.find_element(By.CSS_SELECTOR, 'input#edit-submit-jobs').click()
-    
-    time.sleep(4)
-
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".views-table.views-view-table.cols-3 tbody tr")
-    
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "td:nth-child(1) a").get_attribute("href").strip()
-        location = "US"
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "td:nth-child(1) a").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job_openings.type-job_openings")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "United Kingdom",
+                    link,
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
