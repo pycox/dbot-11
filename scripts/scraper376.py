06731ecafce6e1d8a019e572228a9c4f76f8de56
Shiva <shivanand.gupta@outlook.com>
2024-08-01 12:29:17 +0530

    Update the scraper 376

--- scripts/scraper376.py
+++ scripts/scraper376.py
@@ -6,22 +6,21 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 376
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     data = []
-    time.sleep(4)
+    time.sleep(8)
     
     items = driver.find_elements(By.CSS_SELECTOR, '.whr-items .whr-item')
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, ".whr-title a").get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, '.whr-location').text.split(":")[-1].strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
