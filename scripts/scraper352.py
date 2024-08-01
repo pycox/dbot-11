05713f3c9b317f0c298578730221341597c36ac5
Shiva <shivanand.gupta@outlook.com>
2024-08-01 12:27:22 +0530

    Update the scraper 352

--- scripts/scraper352.py
+++ scripts/scraper352.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 352
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -21,26 +20,16 @@ def main():
     flag = True
     while flag:
         items = driver.find_elements(By.CSS_SELECTOR, ".col-xs-12.col-sm-6.col-md-3")
-        print(items)
         for item in items:
-            print(item)
             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, ".value_location").text.strip()
-            for str in [
-                "Belfast", "Birmingham", "Bournemouth", "Bristol", "Channel Islands", "Cheltenham", "Edinburgh",
-                "Exeter", "Glasgow", "Guernsey", "Jersey", "Leeds", "Liverpool", "Manchester", "Reading", "Reigate",
-                "Sheffield", 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US'
-            ]:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
         try:
             next_button = driver.find_element(By.CSS_SELECTOR, 'li.next')
