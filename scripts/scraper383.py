180d56655a00c469c2115929208bce29f4c93ed1
Shiva <shivanand.gupta@outlook.com>
2024-08-02 07:35:58 +0530

    Update the scraper 383

--- scripts/scraper383.py
+++ scripts/scraper383.py
@@ -4,11 +4,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def main():
-    key = 383
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,33 +17,33 @@ def main():
 
     time.sleep(4)
 
-    try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-action='click->common--cookies--alert#disableAll']").click()
-    except:
-        print("No Cookie Button")
-
-    time.sleep(4)
-
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, "#jobs_list_container li")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".mt-1.text-md span:nth-child(3)").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a span").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
 
-    driver.quit()
-    updateDB(key, data)
+    if "UK" in locations:
+        iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[width=\"100%\"]")))
+        driver.switch_to.frame(iframe)
+        time.sleep(4)
+        
+        driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, "tbody"))
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+            
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "tbody tr")
+        for item in items:
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "td").text.strip(),
+                    com,
+                    "UK",
+                    item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip(),
+                ]
+            )
+
+        driver.quit()
+        updateDB(key, data)
 
 
 if __name__ == "__main__":
