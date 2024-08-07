8d839b798709c8d28a0f5d8b27a4157e9671d9de
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:34:55 +0530

    Update the scraper 424

--- scripts/scraper424.py
+++ scripts/scraper424.py
@@ -2,42 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 424
-    com, url = readUrl(key)
+
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
-    driver.get(url)
-
-    time.sleep(4)
 
     data = []
+    regions = []
     
-    # flag = True
-    # while flag:
-    #     items = driver.find_elements(By.CSS_SELECTOR, ".list.jobListPanel tbody tr")
-    #     for item in items:
-    #         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #         title = item.find_element(By.CSS_SELECTOR, "td:nth-child(1)").text.strip()
-    #         location = item.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text.strip()
-    #         for str in ['Chicago', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #             if (str in location):
-    #                 data.append(
-    #                     [
-    #                         title,
-    #                         com,
-    #                         location,
-    #                         link,
-    #                     ]
-    #                 )
-    #                 break
+    if "UK" in locations:
+        regions.append(("UK", "5575"))
+    
+    if "US" in locations:
+        regions.append(("US", "5570"))
+    
+    for location, location_code in regions:
+        driver.get(f"{url}&groupType_114={location_code}")
+        time.sleep(4)
+
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, ".cvmJobBoardHeader tr.odd, .cvmJobBoardHeader tr.even")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".jbTableTextStyle").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, 'input[name="next_page"]')
+                driver.execute_script("arguments[0].scrollIntoView();", button)
+                driver.execute_script("arguments[0].click();", button)
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
+
 
     driver.quit()
     updateDB(key, data)
