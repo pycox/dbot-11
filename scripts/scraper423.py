a38de374cf7816a6171c7eb9d97c3445839eb818
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:34:51 +0530

    Update the scraper 423

--- scripts/scraper423.py
+++ scripts/scraper423.py
@@ -2,63 +2,90 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 423
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
+    
     try:
-        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+        driver.find_element(By.CSS_SELECTOR, "#igdpr-reject-button").click()
     except:
         print("No Cookie Button")
-        
+
     data = []
-    
-    iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "iframe")))
-    driver.switch_to.frame(iframe)
-    time.sleep(4)
-    
-    flag = True
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, ".list.jobListPanel tbody tr")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            title = item.find_element(By.CSS_SELECTOR, "td:nth-child(1)").text.strip()
-            location = item.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text.strip()
-            for str in ['Chicago', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            title,
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
+    regions = []
 
+    if "UK" in locations:
+        regions.append("United Kingdom")
+
+    if "US" in locations:
+        regions.append("United States")
+        
+        
+    button = driver.find_element(By.CSS_SELECTOR, "#country-toggle")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(2)
+    button = driver.find_element(By.CSS_SELECTOR, f"input[data-display=\"{regions[0]}\"]")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)   
+    time.sleep(3)
+    button = driver.find_element(By.CSS_SELECTOR, "#country-toggle")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(3)
+    for location in regions:
         try:
-            next_button = driver.find_elements(By.CSS_SELECTOR, '.pagingPanel a')[0]
-            if next_button.text.strip() == "Next":
-                driver.execute_script("arguments[0].click();", next_button)
-                time.sleep(4)
-            else:
-                flag = False
-        except:
-            flag = False
-            print("No More Jobs")
+            button = driver.find_element(By.CSS_SELECTOR, f"input[data-display=\"{location}\"]")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(5)
+        except Exception:
+            continue
+        
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, "ul.job-list li.job-list__item")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
 
+            
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
+                driver.execute_script("arguments[0].scrollIntoView();", next_button)
+                time.sleep(2)
+                if "disabled" in next_button.get_attribute("class"):
+                    flag = False
+                else:
+                    next_button.click()
+                time.sleep(4)
+            except Exception:
+                flag = False
+                print("No More Jobs")
+        
+        try:
+            button = driver.find_element(By.CSS_SELECTOR, f"input[data-display=\"{location}\"]")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(5)
+        except Exception:
+            continue
 
     driver.quit()
     updateDB(key, data)
