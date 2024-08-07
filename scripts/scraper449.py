c53bc424b604c48ae61b2e9e799bff08f933a30d
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:37:08 +0530

    Update the scraper 448

--- scripts/scraper449.py
+++ scripts/scraper449.py
@@ -1,40 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 449
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#accept-cookies').click()
+    except:
+        print("No Cookie Button")
+
     time.sleep(4)
 
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".grid-row.ng-star-inserted a.group")
+    for item in items:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".fa-talos-location-pin+ .ng-star-inserted").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    # items = driver.find_elements(By.CSS_SELECTOR, ".job-post")
-
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-    #     location = item.find_element(By.CSS_SELECTOR, ".office").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, ".name").text.strip(),
-    #                     com,
-    #                     "UK",
-    #                     link,
-    #                 ]
-    #             )
 
     driver.quit()
-
     updateDB(key, data)
 
 
