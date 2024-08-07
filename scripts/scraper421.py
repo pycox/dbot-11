70757c7b841f3534e2009d531bcfcbd7a7469b82
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:34:42 +0530

    Update the scraper 421

--- scripts/scraper421.py
+++ scripts/scraper421.py
@@ -2,41 +2,47 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 421
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
+    
     time.sleep(4)
-
+    
     data = []
-    
-    # items = driver.find_elements(By.CSS_SELECTOR, "a")
-    
-    # for item in items:
-    #     title, location = item.text.replace(">>", "").strip().split("-")
-    #     link = item.get_attribute("href").strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     title,
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
 
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".attrax-vacancy-tile--clifford-chance")
+        for item in items:
+            location = item.find_element(By.CSS_SELECTOR, ".attrax-vacancy-tile__location-freetext .attrax-vacancy-tile__item-value").text.strip()
+            for str in locations:
+                if (str in location):
+                    link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
+                
+        try:
+            button = driver.find_element(By.CSS_SELECTOR, 'li a[aria-label="Next pagination page"]')
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
     driver.quit()
     updateDB(key, data)
