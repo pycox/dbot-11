35654bd6a909dc850bbc2268acbe099aaa23a4bb
Shiva <shivanand.gupta@outlook.com>
2024-08-07 12:06:45 +0530

    Update the scraper 456

--- scripts/scraper456.py
+++ scripts/scraper456.py
@@ -6,27 +6,36 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 456
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(2)
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#onetrust-reject-all-handler').click()
+    except:
+        print("No Cookie Button")
+
+
+    element = driver.find_element(By.CSS_SELECTOR, ".e-vacancies-list")
+    driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)", element)
+
+    time.sleep(4)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".posting")
+    items = driver.find_elements(By.CSS_SELECTOR, ".e-vacancies-list a.e-vacancy-listing")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".sort-by-location.location").text.strip()
-        for str in ['England', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "h5").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
                         com,
                         location,
                         link,
