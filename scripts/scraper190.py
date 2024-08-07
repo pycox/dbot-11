fddce064c2ff876d95681bf25674874083fd18fc
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:30:46 +0530

    Update the scraper 190

--- scripts/scraper190.py
+++ scripts/scraper190.py
@@ -17,27 +17,28 @@ def main(key, com, url, locations):
 
     data = []
 
-    flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            button = driver.find_element(By.CSS_SELECTOR, "#show_more_button")
-            driver.execute_script("arguments[0].scrollIntoView();", button)
-            driver.execute_script("arguments[0].click();", button)
-        except Exception:
-            flag = False
+    if "UK" in locations:
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, "#show_more_button")
+                driver.execute_script("arguments[0].scrollIntoView();", button)
+                driver.execute_script("arguments[0].click();", button)
+            except Exception:
+                flag = False
 
-    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "span")).strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+        items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "span")).strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
