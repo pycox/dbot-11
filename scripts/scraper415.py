b11522ac59872d2f796c773794ac831953a9fa57
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:34:05 +0530

    Update the scraper 415

--- scripts/scraper415.py
+++ scripts/scraper415.py
@@ -6,9 +6,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 415
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,23 +14,30 @@ def main():
 
     time.sleep(4)
 
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#hs-eu-confirmation-button').click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(4)
+
     data = []
     
-    # items = driver.find_elements(By.CSS_SELECTOR, "article")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, "h3 small").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "h3 strong").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+    items = driver.find_elements(By.CSS_SELECTOR, ".job-posts .fwpl-row")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, "div.fwpl-item:last-child").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
 
     driver.quit()
