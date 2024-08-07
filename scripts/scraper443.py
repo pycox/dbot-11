b7fbf13a64b30d22c359f8a3464d59a5bc063cfb
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:37 +0530

    Update the scraper 443

--- scripts/scraper443.py
+++ scripts/scraper443.py
@@ -1,34 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 443
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     time.sleep(4)
-
-    items = driver.find_elements(By.CSS_SELECTOR, "div.wpb_column.vc_column_container.us_animate_this.has_bg_color.start")
-
     data = []
 
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".item")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'p').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        location = item.find_element(By.CSS_SELECTOR, ".location p").text.strip()
+        print(location)
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
                         com,
                         location,
                         link,
@@ -36,8 +35,8 @@ def main():
                 )
                 break
 
-    driver.quit()
 
+    driver.quit()
     updateDB(key, data)
 
 
