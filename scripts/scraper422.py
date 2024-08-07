b6c01dd79fa67b0f671552a56f094c1f53325961
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:34:47 +0530

    Update the scraper 422

--- scripts/scraper422.py
+++ scripts/scraper422.py
@@ -6,9 +6,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 422
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,23 +14,36 @@ def main():
 
     time.sleep(4)
 
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.c-btn.js-cookie-banner-accept.mr-2').click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(4)
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".col-sm-12 .result-wrapper")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "h3.result-title a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".result-info").text.split("|")[-1].strip()
-        for str in ['England', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "h3.result-title a").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    if "UK" in locations:
+        button = driver.find_element(By.CSS_SELECTOR, ".selectric-js-vacancy-locations")
+        driver.execute_script("arguments[0].click();", button)
+        button = driver.find_element(By.CSS_SELECTOR, ".selectric-js-vacancy-locations li[data-index=\"2\"]")
+        driver.execute_script("arguments[0].click();", button)
+        button = driver.find_element(By.CSS_SELECTOR, ".c-filter__submit.js-vacancies-filter-submit")
+        driver.execute_script("arguments[0].click();", button)
+
+        time.sleep(4)
+
+    
+        items = driver.find_elements(By.CSS_SELECTOR, ".vacancy")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
 
     driver.quit()
