cc1bc2f964bb0bffcc10d6023cc270036aa03135
Shiva <shivanand.gupta@outlook.com>
2024-08-07 12:06:32 +0530

    Update the scraper 453

--- scripts/scraper453.py
+++ scripts/scraper453.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 453
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,26 +14,31 @@ def main():
 
     time.sleep(4)
 
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#CybotCookiebotDialogBodyButtonDecline').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
 
     data = []
-
-    # items = driver.find_elements(By.CSS_SELECTOR, ".css-1fquksq.e9up25i0")
-    # for item in items:
-    #     link = url
-    #     location = item.find_element(By.CSS_SELECTOR, '.css-11lck9.e9up25i5').text.strip()
-    #     for str in ['LONDON', 'NEW YORK', 'SAN FRANCISCO', 'UNITED STATES', 'UNITED KINGDOM', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, ".css-1wh1a53.e9up25i6").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".ecs-link-wrapper")
+        for item in items:
+            link = item.get_attribute("data-href").strip()
+            data.append(
+                [
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "h3")),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
-
     updateDB(key, data)
 
 
