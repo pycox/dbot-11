ae01bda122f6b9da6ed8f76ac954c4e97f74e3f4
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:31:54 +0530

    Update the scraper 391

--- scripts/scraper391.py
+++ scripts/scraper391.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 391
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -17,36 +16,24 @@ def main():
     time.sleep(4)
 
     try:
-        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+        driver.find_element(By.CSS_SELECTOR, '#cn-accept-cookie').click()
     except:
         print("No Cookie Button")
 
     data = []
     
-    flag = True
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "div.job-listing .job-summary")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "ul.job_listings li.job_listing a")
         for item in items:
-            link = item.find_element(By.CSS_SELECTOR, ".job-summary-heading a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, ".job-summary-location").text.strip()
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
-        try:
-            driver.find_element(By.CSS_SELECTOR, 'li.page-next').click()
-            time.sleep(4)
-        except:
-            flag = False
-            print("No More Jobs")
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
 
     driver.quit()
