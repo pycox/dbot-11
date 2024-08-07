f3829d4dc1743ec51b3d52951753f929d27ddb92
Shiva <shivanand.gupta@outlook.com>
2024-08-07 12:06:59 +0530

    Update the scraper 459

--- scripts/scraper459.py
+++ scripts/scraper459.py
@@ -6,46 +6,41 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 459
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-    
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#wt-cli-accept-all-btn').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
+
     data = []
     
-    # flag = True
+    items = driver.find_elements(By.CSS_SELECTOR, ".tm-card")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".text").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    # while flag:
-    #     items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
-    #     for item in items:
-    #         link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
-    #         try:
-    #             location = item.find_element(By.CSS_SELECTOR, ".css-248241 .css-129m7dg").text.strip()
-    #         except:
-    #             continue
-    #         for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #             if (str in location):
-    #                 data.append(
-    #                     [
-    #                         item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-    #                         com,
-    #                         location,
-    #                         link,
-    #                     ]
-    #                 )
-    #                 break
-
-        # try:
-        #     driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
-        #     time.sleep(4)
-        # except:
-        #     flag = False
-        #     print("No More Jobs")
 
     driver.quit()
     updateDB(key, data)
