9d0eed59b53efae7755a19588554af37fe26009b
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:33:07 +0530

    Update the scraper 404

--- scripts/scraper404.py
+++ scripts/scraper404.py
@@ -6,53 +6,66 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 404
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
+    
     time.sleep(4)
+    data = []
+    regions = []
     
     try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+        driver.find_element(By.CSS_SELECTOR, '#CybotCookiebotDialogBodyButtonDecline').click()
     except:
         print("No Cookie Button")
+    time.sleep(2)
+        
+    if "UK" in locations:
+        regions.append(("United Kingdom", ".CodeListEntryId_10"))
+    if "US" in locations:
+        regions.append(("United States", ".CodeListEntryId_386"))
 
-    time.sleep(4)
+    for location, location_class in regions:
+        button = driver.find_element(By.CSS_SELECTOR, location_class)
+        driver.execute_script("arguments[0].scrollIntoView();", button)
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(4)
+        
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, ".ListGridContainer .rowContainerHolder")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+            
     
-    data = []
-    
-    flag = True
-
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
             try:
-                location = item.find_element(By.CSS_SELECTOR, ".css-248241 .css-129m7dg").text.strip()
-            except:
-                continue
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
+                next_button = driver.find_element(By.CSS_SELECTOR, '.pagingButtons a.scroller_movenext')
+                driver.execute_script("arguments[0].scrollIntoView();", next_button)
+                if next_button.get_attribute("disabled") == "disabled":
+                    flag = False
+                else:
+                    next_button.click()
 
-        try:
-            driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
-            time.sleep(4)
-        except:
-            flag = False
-            print("No More Jobs")
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")      
+        
+        button = driver.find_element(By.CSS_SELECTOR, location_class)
+        driver.execute_script("arguments[0].scrollIntoView();", button)
+        driver.execute_script("arguments[0].click();", button)
+        time.sleep(3)
 
     driver.quit()
     updateDB(key, data)
