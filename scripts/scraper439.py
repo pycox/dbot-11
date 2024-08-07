c94aa95ed6a1e65294d19a79bd5e3c7a7089ddc5
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:14 +0530

    Update the scraper 439

--- scripts/scraper439.py
+++ scripts/scraper439.py
@@ -1,64 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 439
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    flag = True
-    data = []
+    time.sleep(4)
+
+    button = driver.find_element(By.CSS_SELECTOR, ".ot-close-icon , .md\:w-auto")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    driver.execute_script("arguments[0].click();", button)
+    button.click()
 
     time.sleep(4)
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Yes, I agree"]').click()
-    except:
-        print("No Cookie Button")
 
-    while flag:
-        time.sleep(4)
-        items = driver.find_elements(By.CSS_SELECTOR, ".wpb_column.vc_column_container.our_jobs_item")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, '.joblocation').text.strip()
-            if location in ["New York"]:
-                location = f"{location}, US"
-            elif location in [
-                "Belfast", "Birmingham", "Bournemouth", "Bristol", "Channel Islands", "Cheltenham", "Edinburgh",
-                "Exeter", "Glasgow", "Guernsey", "Jersey", "Leeds", "Liverpool", "Manchester", "Reading", "Reigate",
-                "Sheffield", 
-            ]:
-                location = f"{location}, UK"
-            elif any(substring in location for substring in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']):
-                pass
-            else:
-                continue
-            data.append(
-                [
-                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                    com,
-                    location,
-                    link,
-                ]
-            )
-
-        try:
-          driver.find_element(By.CSS_SELECTOR, "main#main a.next.page-numbers > span").click()
-          time.sleep(4)
-        except:
-          flag = False
-          print("No More Jobs")
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".border-dotted")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".mb-3").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
 
     driver.quit()
-
     updateDB(key, data)
 
 
