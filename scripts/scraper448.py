8a9bc6f70d82c669504cc9e7c51a3043f5b6077c
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:37:03 +0530

    Update the scraper 448

--- scripts/scraper448.py
+++ scripts/scraper448.py
@@ -1,40 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 448
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    
+
     time.sleep(4)
-
+    
     data = []
-
-    items = driver.find_elements(By.CSS_SELECTOR, ".job-post")
-
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        location = item.find_element(By.CSS_SELECTOR, ".office").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, ".name").text.strip(),
-                        com,
-                        "UK",
-                        link,
-                    ]
-                )
+    
+    if "US" in locations:
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+        
+        
+        while True:
+            try:
+                buttons = driver.find_elements(By.CSS_SELECTOR, ".link.details-desc.js-more")
+                if not buttons:
+                    break
+                for button in buttons:
+                    driver.execute_script("arguments[0].scrollIntoView();", button)
+                    driver.execute_script("arguments[0].click();", button)
+                    time.sleep(2)
+            except:
+                break
+        
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "li.opening-job")
+        for item in items:
+            try:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            except:
+                continue
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                    com,
+                    "US",
+                    link,
+                ]
+            )
 
     driver.quit()
-
     updateDB(key, data)
 
 
