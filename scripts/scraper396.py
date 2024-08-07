0284c621036ef8eaf7cc9d8c087fff2cacb536b5
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:32:18 +0530

    Update the scraper 396

--- scripts/scraper396.py
+++ scripts/scraper396.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 396
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,44 +15,22 @@ def main():
 
     time.sleep(4)
 
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'button#system-ialert-button').click()
-    except:
-        print("No Cookie Button")
-
-    time.sleep(4)
-
     data = []
-
-    flag = True
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "#search-results-list .list-item--card")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, ".job-location").text.strip()
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
-        try:
-            next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
-            if "disable" in next_button.get_attribute("class"):
-                flag = False
-            else:
-                next_button.click()
-            time.sleep(4)
-        except:
-            flag = False
-            print("No More Jobs")
-
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "#js-job-list tr")
+    for item in items:
+        location = item.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip(),
+                    ]
+                )
+                break
 
     driver.quit()
     updateDB(key, data)
