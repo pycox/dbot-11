c9a034bf30b21e182b180f432a0740e94523eee4
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:32:58 +0530

    Update the scraper 402

--- scripts/scraper402.py
+++ scripts/scraper402.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 402
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -18,15 +17,15 @@ def main():
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".opening")
+    items = driver.find_elements(By.CSS_SELECTOR, "a.jobListing")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        link = item.get_attribute("href").strip()
         location = item.find_element(By.CSS_SELECTOR, ".location").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, ".title .content").text.strip(),
                         com,
                         location,
                         link,
