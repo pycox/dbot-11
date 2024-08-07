ff38c9c309d9e55f758afd79861949dcc9679752
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:35:20 +0530

    Update the scraper 429

--- scripts/scraper429.py
+++ scripts/scraper429.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 429
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -15,25 +14,26 @@ def main():
 
     time.sleep(4)
 
-    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
-
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".rt-tbody .rt-tr-group")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_elements(By.CSS_SELECTOR, "div[role=\"gridcell\"]")[2].text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
-
-    #     data.append(
-    #         [
-    #             item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
-    #             com,
-    #             location,
-    #             link,
-    #         ]
-    #     )
 
     driver.quit()
-
     updateDB(key, data)
 
 
