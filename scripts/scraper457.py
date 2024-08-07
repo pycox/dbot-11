d8bdd770db3b54db3da095db70da35c1201e0576
Shiva <shivanand.gupta@outlook.com>
2024-08-07 12:06:49 +0530

    Update the scraper 457

--- scripts/scraper457.py
+++ scripts/scraper457.py
@@ -6,53 +6,34 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 457
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-    
+
     try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+        driver.find_element(By.CSS_SELECTOR, 'button[data-action="click->common--cookies--alert#disableAll"]').click()
     except:
         print("No Cookie Button")
-
-    time.sleep(4)
     
+    time.sleep(2)
     data = []
     
-    flag = True
-
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "#jobs_list_container li a")
         for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
-            try:
-                location = item.find_element(By.CSS_SELECTOR, ".css-248241 .css-129m7dg").text.strip()
-            except:
-                continue
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
-        try:
-            driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
-            time.sleep(4)
-        except:
-            flag = False
-            print("No More Jobs")
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "span.company-link-style").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
