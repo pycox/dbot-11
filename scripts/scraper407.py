b0232390ae056572669aa802745327f19a70f0c4
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:33:23 +0530

    Update the scraper 407

--- scripts/scraper407.py
+++ scripts/scraper407.py
@@ -2,48 +2,55 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 407
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
+    
     try:
-        driver.find_element(By.CSS_SELECTOR, 'button.onetrust-close-btn-handler').click()
+        driver.find_element(By.CSS_SELECTOR, "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
     except:
         print("No Cookie Button")
 
+    time.sleep(4)
+    
     data = []
     
-    iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "grnhse_iframe")))
-    driver.switch_to.frame(iframe)
-    
-    items = driver.find_elements(By.CSS_SELECTOR, ".opening")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".location").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    flag = True
 
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job-search .featured-jobs-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a.apply-link").get_attribute("href").strip()
+            location = item.find_elements(By.CSS_SELECTOR, "span.job-info")[1].text.split(":")[-1].strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, ".job-title").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
+
+        try:
+            button = driver.find_element(By.CSS_SELECTOR, 'a[rel="next"]')
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(4)
+        except:
+            flag = False
+            print("No More Jobs")
 
     driver.quit()
     updateDB(key, data)
