1b93def8575755ceed24b64c9ead76d28f537e21
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:58 +0530

    Update the scraper 447

--- scripts/scraper447.py
+++ scripts/scraper447.py
@@ -1,38 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 447
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    
+
+    time.sleep(4)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(4)
 
     data = []
-
-    items = driver.find_elements(By.CSS_SELECTOR, "tr.base-job")
-
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".job-posts--table tbody tr.job-post")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".body.body__secondary.body--metadata").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".body.body--medium").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "td").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
 
     driver.quit()
-
     updateDB(key, data)
 
 
