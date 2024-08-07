6db03974aedbe478437fddbe1c388ba203b7d6bd
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:03 +0530

    Update the scraper 437

--- scripts/scraper437.py
+++ scripts/scraper437.py
@@ -6,53 +6,47 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 437
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-    
+
     try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+        driver.find_element(By.CSS_SELECTOR, 'button#c-p-bn').click()
     except:
         print("No Cookie Button")
 
-    time.sleep(4)
-    
     data = []
     
     flag = True
-
     while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
-            try:
-                location = item.find_element(By.CSS_SELECTOR, ".css-248241 .css-129m7dg").text.strip()
-            except:
-                continue
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
+        time.sleep(4)
         try:
-            driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
-            time.sleep(4)
-        except:
+            button = driver.find_element(By.CSS_SELECTOR, ".vacancies__load-more button")
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            driver.execute_script("arguments[0].click();", button)
+        except Exception:
             flag = False
-            print("No More Jobs")
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".vacancy-card")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".vacancy-card__location").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
 
     driver.quit()
     updateDB(key, data)
