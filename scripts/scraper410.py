901e83d9e15f7af1df86a7baaae2a1e38ed58165
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:33:40 +0530

    Update the scraper 410

--- scripts/scraper410.py
+++ scripts/scraper410.py
@@ -6,9 +6,8 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 410
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,32 +15,22 @@ def main():
         url = url[:-1]
     driver.get(url)
 
-    time.sleep(2)
-    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, "#careers_recentJobPosts"))
     time.sleep(4)
 
     data = []
     
-    flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            driver.find_element(By.CSS_SELECTOR, "div#careers_recentJobPosts button").click()
-        except Exception:
-            flag = False
-    
-    items = driver.find_elements(By.CSS_SELECTOR, "div#careers_recentJobPosts a")
-    for item in items:
-        link = item.get_attribute("href").strip()
-        location = "London"
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "p.heading-4").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+
+    if "UK" in locations:    
+        items = driver.find_elements(By.CSS_SELECTOR, ".jobListing__container a.jobPreview")
+        for item in items:
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, ".xsTitle").text.strip(),
+                    com,
+                    "UK",
+                    item.get_attribute("href"),
+                ]
+            )
 
 
     driver.quit()
