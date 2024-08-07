32a823d12775e7205e597ddffc1e73189a61d134
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:33:50 +0530

    Update the scraper 412

--- scripts/scraper412.py
+++ scripts/scraper412.py
@@ -6,20 +6,12 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 412
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
-    time.sleep(4)
-    
-    try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
-    except:
-        print("No Cookie Button")
 
     time.sleep(4)
     
@@ -28,18 +20,15 @@ def main():
     flag = True
 
     while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+        items = driver.find_elements(By.CSS_SELECTOR, ".applications-content a.applications-row")
         for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
-            try:
-                location = item.find_element(By.CSS_SELECTOR, ".css-248241 .css-129m7dg").text.strip()
-            except:
-                continue
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            link = item.get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".applications-where").text.strip()
+            for str in locations:
                 if (str in location):
                     data.append(
                         [
-                            item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                            item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
                             com,
                             location,
                             link,
@@ -48,7 +37,11 @@ def main():
                     break
 
         try:
-            driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
+            button = driver.find_elements(By.CSS_SELECTOR, '.pager-row tr td')[-2].find_element(By.CSS_SELECTOR, 'a')
+            if button.text.strip() == "Next":
+                button.click()
+            else:
+                flag = False
             time.sleep(4)
         except:
             flag = False
