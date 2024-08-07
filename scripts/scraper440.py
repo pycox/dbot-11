32af001b6810df30f95c4e7e772ffdf1f96f4d30
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:23 +0530

    Update the scraper 440

--- scripts/scraper440.py
+++ scripts/scraper440.py
@@ -1,53 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 440
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(4)
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(4)
+    
     data = []
 
-    time.sleep(4)
+    if "UK" in locations:
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        "UK",
+                        link,
+                    ]
+                )
 
-    time.sleep(4)
-    items = driver.find_elements(By.CSS_SELECTOR, ".open-position-listing.w-dyn-item")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, '.open-position-item-wrapper:nth-child(3)').text.strip()
-        if location in ["New York"]:
-            location = f"{location}, US"
-        elif location in [
-            "Belfast", "Birmingham", "Bournemouth", "Bristol", "Channel Islands", "Cheltenham", "Edinburgh",
-            "Exeter", "Glasgow", "Guernsey", "Jersey", "Leeds", "Liverpool", "Manchester", "Reading", "Reigate",
-            "Sheffield", 
-        ]:
-            location = f"{location}, UK"
-        elif any(substring in location for substring in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']):
-            pass
-        else:
-            continue
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, ".open-position-title").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+            try:
+                driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]').click()
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
 
-
-    driver.quit()
-
-    updateDB(key, data)
-
+        driver.quit()
+        updateDB(key, data)
 
 if __name__ == "__main__":
     main()
