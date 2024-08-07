6bf942d8050860ef854f2e2ab193f4070df7ce00
Shiva <shivanand.gupta@outlook.com>
2024-08-07 10:36:32 +0530

    Update the scraper 442

--- scripts/scraper442.py
+++ scripts/scraper442.py
@@ -6,40 +6,30 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 442
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-    
-    try:
-        driver.find_element(By.CSS_SELECTOR, "a#wt-cli-accept-btn").click()
-    except:
-        print("No Cookie Button")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
 
-    time.sleep(4)
-    
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, "#lever-jobs-container li.lever-job")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "span").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".vidcruiter-job-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
