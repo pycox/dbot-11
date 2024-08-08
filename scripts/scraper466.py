b155c0e7dd963d2833b74b6f182d891b330028e2
Shiva <shivanand.gupta@outlook.com>
2024-08-08 11:27:09 +0530

    Update the scraper 466

--- scripts/scraper466.py
+++ scripts/scraper466.py
@@ -6,41 +6,33 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 466
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
 
     data = []
     
-    # flag = True
-    # while flag:
-    #     time.sleep(4)
-    #     try:
-    #         driver.find_element(By.CSS_SELECTOR, "div.d-flex.mt-4.ng-star-inserted > button").click()
-    #     except Exception:
-    #         flag = False
-    
-    # items = driver.find_elements(By.CSS_SELECTOR, "li.careers-listings-item")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, ".careers-listings-item--location").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+    items = driver.find_elements(By.CSS_SELECTOR, ".whr-items li.whr-item")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".whr-location").text.split("Location:")[-1].strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
 
     driver.quit()
