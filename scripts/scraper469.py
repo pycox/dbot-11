7dadabc1e187a9f96085215e4390976b2193e175
Shiva <shivanand.gupta@outlook.com>
2024-08-08 11:27:23 +0530

    Update the scraper 469

--- scripts/scraper469.py
+++ scripts/scraper469.py
@@ -1,51 +1,59 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 469
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
+    
     try:
-        driver.find_element(By.CSS_SELECTOR, 'button.button.cookie-consent__button:nth-child(1)').click()
+        driver.find_element(By.CSS_SELECTOR, 'button[data-action="click->common--cookies--alert#disableAll"]').click()
     except:
         print("No Cookie Button")
+    time.sleep(3)
 
-    time.sleep(4)
+    flag = True
+    while flag:
+      try:
+        driver.find_element(By.CSS_SELECTOR, 'a#show_more_button').click()
+        time.sleep(4)
+      except:
+        flag = False
+        print("No more Jobs")
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
 
     data = []
 
-    try:
-        driver.find_element(By.CSS_SELECTOR, ".cc-job-list__view-more a").click()
-        time.sleep(4)
-    except Exception:
-        print("No more button")
-
-    items = driver.find_elements(By.CSS_SELECTOR, ".job-tile.job-grid-item")
-    location = "London, UK"
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, ".job-tile__title").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+        try:
+          location = item.find_element(By.CSS_SELECTOR, 'div > span:nth-child(3)').text.strip()
+        except:
+          location = item.find_element(By.CSS_SELECTOR, 'div > span').text.strip()
 
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "span").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
     driver.quit()
+
     updateDB(key, data)
 
 
