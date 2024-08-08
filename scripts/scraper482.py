f5d7efe2ad17c63a5761dfa2cb4acb81f44c3694
Shiva <shivanand.gupta@outlook.com>
2024-08-08 11:28:44 +0530

    Update the scraper 482

--- scripts/scraper482.py
+++ scripts/scraper482.py
@@ -5,44 +5,45 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 482
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
+    
     try:
-        driver.find_element(
-            By.CSS_SELECTOR,
-            "button#onetrust-accept-btn-handler",
-        ).click()
-    except Exception as e:
-        print(f"Scraper{key} cookiee button: {e}")
+        driver.find_element(By.CSS_SELECTOR, 'button[data-action="click->common--cookies--alert#disableAll"]').click()
+    except:
+        print("No Cookie Button")
+    time.sleep(3)
 
-    time.sleep(4)
+    flag = True
+    while flag:
+      try:
+        driver.find_element(By.CSS_SELECTOR, 'a#show_more_button').click()
+        time.sleep(4)
+      except:
+        flag = False
+        print("No more Jobs")
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li")
 
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.card-body")
-
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        location = item.find_element(
-            By.XPATH, "//div[small[text()='Location']]/following-sibling::div"
-        ).text.strip()
-
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
-                com,
-                location,
-                link,
-            ]
-        )
+    if "UK" in locations:
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "span").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
 
