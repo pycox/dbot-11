1c04ddafab7574a3790555b88b49903cf0150e90
Shiva <shivanand.gupta@outlook.com>
2024-08-08 11:28:04 +0530

    Update the scraper 473

--- scripts/scraper473.py
+++ scripts/scraper473.py
@@ -1,32 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 473
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(8)
+    time.sleep(4)
 
-    items = driver.find_elements(By.CSS_SELECTOR, "div.grid-container.feed")
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.cmplz-btn.cmplz-deny').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4)
 
     data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "#filter_global-e a.e-parent")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, 'div.feed-item-location').text.strip()
-
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str.lower() in location.lower()):
+        link = item.get_attribute("href").strip()
+        try:
+            location = item.find_elements(By.CSS_SELECTOR, "span")[1].text.strip()
+        except:
+            continue
+        for str in locations:
+            if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, ".elementor-heading-title").text.strip(),
                         com,
                         location,
                         link,
@@ -34,8 +43,8 @@ def main():
                 )
                 break
 
-    driver.quit()
 
+    driver.quit()
     updateDB(key, data)
 
 
