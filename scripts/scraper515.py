e27d8fb9d0385479612c49f248e9b5ff9dddd520
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:21:39 +0530

    Update the scraper 515

--- scripts/scraper515.py
+++ scripts/scraper515.py
@@ -6,29 +6,44 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 515
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    time.sleep(2)
-    driver.get(url)
-    time.sleep(3)
-
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".careers-list__row")[1:]
+    time.sleep(4)
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#hs-eu-confirmation-button').click()
+    except:
+        print("No Cookie Button")
+    time.sleep(2)
+    
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                flag = False
+        except:
+            flag = False
+
+    items = driver.find_elements(By.CSS_SELECTOR, ".container-jobs .item-job")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".careers-list__col:nth-child(3)").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+        location = item.find_element(By.CSS_SELECTOR, ".city-job p").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, ".careers-list__col:nth-child(1)").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "h5").text.strip(),
                         com,
                         location,
                         link,
