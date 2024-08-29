b1c40f973e4ce1c326d76da8150a7accda1e4fd9
Shiva <shivanand.gupta@outlook.com>
2024-08-29 15:00:28 +0530

    Add the scraper 560

---
+++ scripts/scraper560.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.cookie-button').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
+    data = []
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".jk-job-wrapper")
+        for item in items:
+            item = item.find_element(By.XPATH, "./..")
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, ".title").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
