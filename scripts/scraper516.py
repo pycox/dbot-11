a0e26184b8129ed22733148b5cb66c8efc3597b3
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:21:43 +0530

    Update the scraper 516

--- scripts/scraper516.py
+++ scripts/scraper516.py
@@ -1,53 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 516
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-
-    time.sleep(2)
-
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'button#ccc-recommended-settings').click()
-    except:
-        print("No Cookie Button")
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    time.sleep(2)
-
-    data = []
     
     flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            driver.find_element(By.CSS_SELECTOR, "button#tile-more-results").click()
-        except Exception:
-            flag = False
-    
-    items = driver.find_elements(By.CSS_SELECTOR, ".job-tile-cell")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "United Kingdom",
-                link,
-            ]
-        )
+    data = []
+
+    if "UK" in locations:
+        while flag:
+            try:
+                time.sleep(4)
+
+                items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+
+                for item in items:
+                    link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                            com,
+                            "UK",
+                            link,
+                        ]
+                    )
+
+                if (
+                    len(
+                        driver.find_elements(
+                            By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                        )
+                    )
+                    > 0
+                ):
+                    driver.find_element(
+                        By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                    ).click()
+                else:
+                    flag = False
+                    break
+
+            except Exception as e:
+                flag = False
 
     driver.quit()
+
     updateDB(key, data)
 
 
