b1ff9e0a7d60641ed4f8445b32987b6f4fdc8eb7
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:24:11 +0530

    Update the scraper 545

--- scripts/scraper545.py
+++ scripts/scraper545.py
@@ -6,47 +6,42 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 545
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(4)
-
     data = []
     
-    flag = True
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, ".jobs-list app-job-card")
-        if not items:
-            flag = False
-            continue
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-            location = item.find_element(By.CSS_SELECTOR, ".job-card-location").text.strip()
-            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-                if (str in location):
-                    data.append(
-                        [
-                            item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                            com,
-                            location,
-                            link,
-                        ]
-                    )
-                    break
-
-        try:
-            driver.find_element(By.CSS_SELECTOR, 'button.o-buttons-icon--arrow-right.o-buttons-icon--icon-only').click()
+    time.sleep(4)
+    
+    if "UK" in locations:
+        flag = True
+        while flag:
             time.sleep(4)
-        except:
-            flag = False
-            print("No More Jobs")
+            try:
+                last_height = driver.execute_script("return document.body.scrollHeight")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(4)
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    flag = False
+            except:
+                flag = False
+
+        items = driver.find_elements(By.CSS_SELECTOR, "#results .vacancy-search-result-item a.apply-link")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
 
     driver.quit()
