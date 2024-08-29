a6b1706802f6cff15f67d72a44042e74b43b2d27
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:23:57 +0530

    Update the scraper 542

--- scripts/scraper542.py
+++ scripts/scraper542.py
@@ -5,28 +5,42 @@ from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def main():
-    key = 542
-    com, url = readUrl(key)
+
+
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    time.sleep(3)
+
+    try:
+        iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "iframe")))
+        driver.switch_to.frame(iframe)
+        driver.find_element(By.CSS_SELECTOR, 'button.oracletaleocwsv2-btn-fa.fa-search').click()
+        time.sleep(3)
+    except:
+        print("No Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".editorial-card.processed")
+    items = driver.find_elements(By.CSS_SELECTOR, ".oracletaleocwsv2-accordion-head-info")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "small").text
-        for str in ['LONDON', 'NEW YORK', 'SAN FRANCISCO', 'UNITED STATES', 'UNITED KINGDOM']:
+        location = item.find_element(By.CSS_SELECTOR, "div[tabindex=\"0\"]").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
                         com,
                         location,
                         link,
