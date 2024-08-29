8006ca9e066dd82020f830101ab7cd5f8bb3dcd1
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:21:17 +0530

    Update the scraper 510

--- scripts/scraper510.py
+++ scripts/scraper510.py
@@ -6,29 +6,49 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 510
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(2)
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#onetrust-reject-all-handler').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".text-image-2__content.flex.justify-center")
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            driver.find_element(By.CSS_SELECTOR, "button[title=\"See more\"]").click()
+        except Exception:
+            flag = False
+    
+    items = driver.find_elements(By.CSS_SELECTOR, "a[data-testid=\"jobListItem\"]")
     for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+        link = item.get_attribute("href").strip()
+        location = item.find_elements(By.CSS_SELECTOR, "span")[1].text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
 
     driver.quit()
     updateDB(key, data)
