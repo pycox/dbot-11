2ce870d2fea083426d5df960b1dbe8e8c7e208c5
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:19:59 +0530

    Update the scraper 495

--- scripts/scraper495.py
+++ scripts/scraper495.py
@@ -6,63 +6,43 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 495
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-    
+    time.sleep(3)
+
     try:
-        driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='legalNoticeDeclineButton']").click()
+        driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Accept All"]').click()
     except:
         print("No Cookie Button")
+
     data = []
+    
+    if "UK" in locations:
         
-    for location, location_id in [("United States", "bc33aa3152ec42d4995f4791a106ed09"), ("United Kingdom", "29247e57dbaf46fb855b224e03170bc7")]:
-        try:
-            driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='distanceLocation']").click()
-            time.sleep(2)
-            driver.find_element(By.CSS_SELECTOR, "label[for='location']").click()
-            time.sleep(2)
-            driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, f"input#{location_id}"))
-            driver.find_element(By.CSS_SELECTOR, "div[data-automation-id='sidebar'").click()
-            
+        page = 1
+        items = True
+
+        while items:
+            driver.get(f"{url}?page={page}")
             time.sleep(3)
-        except:
-            continue
 
-        flag = True
-
-        while flag:
-            items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+            items = driver.find_elements(By.CSS_SELECTOR, "#search-result .border-slate-1")
             for item in items:
-                link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
+                link = item.find_element(By.CSS_SELECTOR, "a[aria-label=\"Job detail page\"]").get_attribute("href").strip()
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "p.tt-jobName").text.strip(),
                         com,
-                        location,
+                        "UK",
                         link,
                     ]
                 )
 
-            
-            try:
-                driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, 'button[data-uxi-element-id="next"]'))
-                
-                time.sleep(4)
-            except Exception:
-                flag = False
-                print("No More Jobs")
-        
-        try:
-            driver.find_element(By.CSS_SELECTOR, 'button[data-automation-id="clearAllButton"]').click()
-        except:
-            print("No filter to clear")
+            page += 1
 
     driver.quit()
     updateDB(key, data)
