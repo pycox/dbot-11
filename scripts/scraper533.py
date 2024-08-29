ef8ebaea86f78fcec13e50f6fae24a186927b3ee
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:23:11 +0530

    Update the scraper 533

--- scripts/scraper533.py
+++ scripts/scraper533.py
@@ -6,31 +6,54 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 533
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(3)
+
     data = []
+    regions = []
+
+    if "UK" in locations:
+        regions.append((None, "United Kingdom"))
     
-    # items = driver.find_elements(By.CSS_SELECTOR, "ul .css-zqnl66 > div")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, ".css-gs8ao5").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+    if "US" in locations:
+        regions.append((233, "United State"))    
+    
+    for location_code, location in regions:
+        if not location_code:
+            continue
+        
+        driver.get(f"{url}?&splitLocationSearch_country={location_code}")
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        
+        flag = True
+        while flag:
+            items = driver.find_elements(By.CSS_SELECTOR, ".article.article--result h3 a")
+            for item in items:
+                data.append(
+                    [
+                        item.text.strip(),
+                        com,
+                        location,
+                        item.get_attribute("href").strip(),
+                    ]
+                )
+            
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, 'a.list-controls__pagination__item.paginationNextLink')
+                driver.execute_script("arguments[0].scrollIntoView();", button)
+                driver.execute_script("arguments[0].click();", button)
+                time.sleep(4)
+            except:
+                flag = False
+                print("No More Jobs")
+
 
     driver.quit()
     updateDB(key, data)
