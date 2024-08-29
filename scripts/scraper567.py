6c93dc881a93c4ecfc0c971af64bff3a788bc93a
Shiva <shivanand.gupta@outlook.com>
2024-08-29 15:01:01 +0530

    Add the scraper 567

---
+++ scripts/scraper567.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, "button[data-action='click->common--cookies--alert#disableAll']").click()
+    except:
+        print("No Cookie Button")
+
+    data = []
+    
+    if "UK" in locations:
+        
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                driver.find_element(By.CSS_SELECTOR, "a#show_more_button").click()
+            except Exception:
+                flag = False
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container li")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "span.text-block-base-link.company-link-style")).strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
