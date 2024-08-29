5a37be56697a26a6fc1963ba6da72ffedf6aa50f
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:19:13 +0530

    Update the scraper 486

--- scripts/scraper486.py
+++ scripts/scraper486.py
@@ -6,44 +6,49 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 386
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    time.sleep(3)
 
     try:
-        driver.find_element(By.CSS_SELECTOR, "button#truste-consent-button").click()
+        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
     except:
         print("No Cookie Button")
 
-    time.sleep(4)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
 
     data = []
-    # driver.execute_script("document.querySelectorAll('.accordion-content').forEach(item => item.classList.add('open'));")
-
-    # time.sleep(4)
-
     
-    # items = driver.find_elements(By.CSS_SELECTOR, ".accordion-content ul li.job")
-    # for item in items:
-    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-    #     location = item.find_element(By.CSS_SELECTOR, "a h3 small").text.strip()
-    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #         if (str in location):
-    #             data.append(
-    #                 [
-    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-    #                     com,
-    #                     location,
-    #                     link,
-    #                 ]
-    #             )
-    #             break
+    if "UK" in locations:
+        
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                button = driver.find_element(By.CSS_SELECTOR, "#show-more-button")
+                driver.execute_script("arguments[0].scrollIntoView();", button)
+                driver.execute_script("arguments[0].click();", button)
+            except Exception:
+                flag = False
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "#talentlinkJobsList .job-list-tile")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
 
     driver.quit()
     updateDB(key, data)
