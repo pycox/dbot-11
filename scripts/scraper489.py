2e91c0701adf51018f40c25c6f3eec3af8dc5493
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:19:31 +0530

    Update the scraper 489

--- scripts/scraper489.py
+++ scripts/scraper489.py
@@ -6,34 +6,36 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 489
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-    
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'Button#ccc-recommended-settings').click()
-    except:
-        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, "div#block-iab-content li a")
+    items = driver.find_elements(By.CSS_SELECTOR, ".sm\:flex-row")
     for item in items:
-        link = item.get_attribute("href").strip()
-        data.append(
-            [
-                item.text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".text-gray-400").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
 
     driver.quit()
     updateDB(key, data)
