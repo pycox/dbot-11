af5eed78763158e488d733ddfa3a25d4c712e8d1
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:24:15 +0530

    Update the scraper 546

--- scripts/scraper546.py
+++ scripts/scraper546.py
@@ -6,42 +6,31 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 546
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
-    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
-    
-    data = []
-        
-    flag = True
+    time.sleep(3)
 
-    while flag:
-        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
-        for item in items:
-            link = item.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("href").strip()
-            data.append(
+    data = []
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".vc_column-inner .wpb_wrapper .wpb_row.vc_row-fluid.vc_row.inner_row")
+        for item in items[:-1]:
+            item = item.find_element(By.XPATH, "./..") 
+            link = item.find_element(By.CSS_SELECTOR, "a.nectar-button.large.regular.accent-color.regular-button").get_attribute("href").strip()
+            data.append(    
                 [
-                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, "h3")).strip(),
                     com,
                     "UK",
                     link,
                 ]
             )
 
-        
-        try:
-            driver.execute_script("arguments[0].click();", driver.find_element(By.CSS_SELECTOR, 'button[data-uxi-element-id="next"]'))
-            time.sleep(4)
-        except Exception:
-            flag = False
-            print("No More Jobs")
-    
+
     driver.quit()
     updateDB(key, data)
 
