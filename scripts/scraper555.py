76d599170457a7d06962989c69ea058b6c708c16
Shiva <shivanand.gupta@outlook.com>
2024-08-29 14:59:07 +0530

    Update the scraper 555

--- scripts/scraper555.py
+++ scripts/scraper555.py
@@ -6,9 +6,7 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 555
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -16,19 +14,27 @@ def main():
 
     time.sleep(4)
 
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(2)
+
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".fabric-5qovnk-root.MuiBox-root.css-ldt7kr")
+    items = driver.find_elements(By.CSS_SELECTOR, ".postings-group .posting")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
+        location = item.find_element(By.CSS_SELECTOR, ".location").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h5").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
 
 
     driver.quit()
