fe09f9df0ca2e9abbe9f56ae2257f45c91fdbe85
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:22:40 +0530

    Update the scraper 527

--- scripts/scraper527.py
+++ scripts/scraper527.py
@@ -6,27 +6,38 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 527
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button.cmplz-btn.cmplz-accept').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".job_openings.type-job_openings")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
-                com,
-                "United Kingdom",
-                link,
-            ]
-        )
+    if "UK" in locations:
+        
+        items = driver.find_elements(By.CSS_SELECTOR, ".pp-content-posts .pp-content-post")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
 
     driver.quit()
