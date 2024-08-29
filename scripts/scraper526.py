01e4cbcb571835b7efa9d202ded5ae0e82962142
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:22:35 +0530

    Update the scraper 526

--- scripts/scraper526.py
+++ scripts/scraper526.py
@@ -4,43 +4,53 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def main():
-    key = 526
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(3)
+
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button[data-role="all"]').click()
+    except:
+        print("No Cookie Button")
+
+    iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#gnewtonIframe")))
+    driver.switch_to.frame(iframe)
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
     data = []
     
-    flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            last_height = driver.execute_script("return document.body.scrollHeight")
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                flag = False
-        except:
-            flag = False
-    
-    items = driver.find_elements(By.CSS_SELECTOR, ".search-results.job-tile.job-list-item")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "span.job-tile__title").text.strip(),
-                com,
-                "United Kingdom",
-                link,
-            ]
-        )
-
+    regions = driver.find_elements(By.CSS_SELECTOR, ".gnewtonCareerGroupHeaderClass")
+    for region in regions:
+        location = region.text.strip()
+        for str in locations:
+            if (str in location):
+                sibling = region.find_element(By.XPATH, "./following-sibling::*[contains(@class, 'gnewtonCareerGroupRowClass')]")
+                while sibling and 'gnewtonCareerGroupRowClass' in sibling.get_attribute('class'):
+                    link = sibling.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+                    data.append(
+                        [
+                            sibling.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    sibling = sibling.find_element(By.XPATH, "./following-sibling::*[contains(@class, 'gnewtonCareerGroupRowClass') or contains(@class, 'gnewtonCareerGroupHeaderClass')]")
+                    if sibling and 'gnewtonCareerGroupHeaderClass' in sibling.get_attribute('class'):
+                        break
+                break
 
     driver.quit()
     updateDB(key, data)
