dc5aec8890e36c276d3e6b58305062de587e1ac0
Shiva <shivanand.gupta@outlook.com>
2024-08-29 14:58:56 +0530

    Update the scraper 553

--- scripts/scraper553.py
+++ scripts/scraper553.py
@@ -2,15 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 553
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -18,20 +15,39 @@ def main():
 
     time.sleep(4)
 
+    try:
+        driver.find_element(By.CSS_SELECTOR, "button[data-action='click->common--cookies--alert#disableAll']").click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(2)
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, ".jobs-list-container"))
+    element = driver.find_element(By.CSS_SELECTOR, '.jobs-list-container')
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;", element)
+    time.sleep(2)
+
     data = []
-
-    items = driver.find_elements(By.CSS_SELECTOR, ".opening")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                com,
-                "UK",
-                link,
-            ]
-        )
-
+    
+    if "UK" in locations:
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                driver.find_element(By.CSS_SELECTOR, "a#show_more_button").click()
+            except Exception:
+                flag = False
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container li")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    driver.execute_script("return arguments[0].innerText;", item.find_element(By.CSS_SELECTOR, ".text-block-base-link.company-link-style")).strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
