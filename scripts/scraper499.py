8bf1a03406f8a5cfea5960a129b8db5dc57efe73
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:20:18 +0530

    Update the scraper 499

--- scripts/scraper499.py
+++ scripts/scraper499.py
@@ -2,44 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 499
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     time.sleep(4)
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button#CybotCookiebotDialogBodyButtonDecline').click()
+    except:
+        print("No Cookie Button")
     time.sleep(4)
-    
+
     data = []
-    iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ashby_embed_iframe")))
-    driver.switch_to.frame(iframe)
     
-    items = driver.find_elements(By.CSS_SELECTOR, "._container_j2da7_1")
-    for item in items:
-        link = item.get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "p").text.split("•")[1].strip()
-        for str in ['Bournemouth', 'London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    if "UK" in locations:
+    
+        items = driver.find_elements(By.CSS_SELECTOR, ".rt-list__items .rt-list__offer-item")
+        for item in items:
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, ".rt-list__offer-title").text.strip(),
+                    com,
+                    "UK",
+                    url,
+                ]
+            )
 
 
     driver.quit()
