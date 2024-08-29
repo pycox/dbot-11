ba55c3ff345d6d06cd441b22d0b5e56b256971a1
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:22:50 +0530

    Update the scraper 529

--- scripts/scraper529.py
+++ scripts/scraper529.py
@@ -6,35 +6,40 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 529
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
-    options.add_argument("--log-level=3")
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button.cky-btn.cky-btn-reject').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
     data = []
     
-    # flag = True
-    # while flag:
-    #     items = driver.find_elements(By.CSS_SELECTOR, ".results-item.vacancy")
-    #     for item in items:
-    #         link = item.find_element(By.CSS_SELECTOR, ".vacancy__title a").get_attribute("href").strip()
-    #         location = item.find_element(By.CSS_SELECTOR, ".vacancy__region").text.strip()
-    #         for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-    #             if (str in location):
-    #                 data.append(
-    #                     [
-    #                         item.find_element(By.CSS_SELECTOR, ".vacancy__title").text.strip(),
-    #                         com,
-    #                         location,
-    #                         link,
-    #                     ]
-    #                 )
-    #                 break
-
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.elementor-icon-list-item a")
+        for item in items:
+            try:
+                title = item.find_element(By.CSS_SELECTOR, "span b").text.strip()
+            except:
+                continue
+            data.append(
+                [
+                    title,
+                    com,
+                    "UK",
+                    item.get_attribute("href").strip(),
+                ]
+            )
 
     driver.quit()
     updateDB(key, data)
