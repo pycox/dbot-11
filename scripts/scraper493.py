42838072f25049259b412310913af0a62f3f04d4
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:19:50 +0530

    Update the scraper 493

--- scripts/scraper493.py
+++ scripts/scraper493.py
@@ -2,15 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 493
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -19,22 +15,40 @@ def main():
     time.sleep(4)
 
     data = []
+    
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+    except:
+        print("No Cookie Button")
+    
+    
+    flag = True
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, ".ais-Hits-list li.ais-Hits-item")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, ".ga-jobLocation").text.strip()
+            for str in locations:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, ".eiwIDV").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
 
-    items = driver.find_elements(By.CSS_SELECTOR, ".opening")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".location").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+        try:
+            curr_button = int(driver.find_element(By.CSS_SELECTOR, '.ais-Pagination-item--selected a').text.strip())
+            next_button = driver.find_element(By.CSS_SELECTOR, f'.ais-Pagination-item.ais-Pagination-item--page a[aria-label="Page {curr_button+1}"]')
+            driver.execute_script("arguments[0].scrollIntoView();", next_button)
+            driver.execute_script("arguments[0].click();", next_button)
+            time.sleep(4)
+        except Exception:
+            flag = False
+            print("No More Jobs")
 
 
     driver.quit()
