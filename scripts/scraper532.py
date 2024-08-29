ecd5c0e7fd495d1a3eaa71cea8d4b77ab8c0ff33
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:23:06 +0530

    Update the scraper 532

--- scripts/scraper532.py
+++ scripts/scraper532.py
@@ -6,33 +6,40 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 532
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(4)
+    time.sleep(3)
 
     data = []
+    regions = []
+
+    if "UK" in locations:
+        regions.append("United Kingdom")
     
-    items = driver.find_elements(By.CSS_SELECTOR, "ul .css-zqnl66 > div")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".css-gs8ao5").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    if "US" in locations:
+        regions.append("United State")    
+    
+    for location in regions:
+        driver.get(f"{url}?&country={location}")
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        
+        items = driver.find_elements(By.CSS_SELECTOR, "ul.positions li.position")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+                    com,
+                    location,
+                    link,
+                ]
+            )
 
 
     driver.quit()
