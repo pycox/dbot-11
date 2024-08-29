9b1204739749e09c0294d06ce58bee40f958d0c6
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:23:48 +0530

    Update the scraper 540

--- scripts/scraper540.py
+++ scripts/scraper540.py
@@ -6,37 +6,36 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 540
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(2)
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#hs-eu-confirmation-button').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
 
     data = []
     
-    flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            driver.find_element(By.CSS_SELECTOR, "#show_more_button").click()
-        except Exception:
-            flag = False
-    
-    items = driver.find_elements(By.CSS_SELECTOR, "#jobs_list_container li")
+    items = driver.find_elements(By.CSS_SELECTOR, ".span3")
     for item in items:
         link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, ".mt-1.text-md").text
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom']:
+        location = item.find_element(By.CSS_SELECTOR, "div[style=\"text-align: left; font-size: 16px;\"]").text.strip()
+        for str in locations:
             if (str in location):
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, "span.company-link-style").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "strong").text.strip(),
                         com,
-                        str,
+                        location,
                         link,
                     ]
                 )
