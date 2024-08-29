7d71e146eb8a28a1e4b4580c652b1a00ab45d21a
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:20:03 +0530

    Update the scraper 496

--- scripts/scraper496.py
+++ scripts/scraper496.py
@@ -1,49 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 496
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
+    if all(loc in locations for loc in ["US", "UK"]):
+        url += "?locationCountry=bc33aa3152ec42d4995f4791a106ed09&locationCountry=29247e57dbaf46fb855b224e03170bc7"
+    elif "US" in locations:
+        url += "?locationCountry=bc33aa3152ec42d4995f4791a106ed09"
+    elif "UK" in locations:
+        url += "?locationCountry=29247e57dbaf46fb855b224e03170bc7"
+
     driver.get(url)
-
-    time.sleep(4)
-
-    try:
-        driver.find_element(By.CSS_SELECTOR, 'button[mode="primary"').click()
-    except:
-        print("No Cookie Button")
-    time.sleep(4)
-
+    flag = True
     data = []
 
-    items = driver.find_elements(By.CSS_SELECTOR, "#tab-annunci .col-12.col-md-6")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, ".vacancy__title a").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "span[title='Location']").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
+    while flag:
+        try:
+            time.sleep(4)
+
+            items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                location = item.find_element(By.CSS_SELECTOR, "dd").text.strip()
+
                 data.append(
                     [
-                        item.find_element(By.CSS_SELECTOR, ".vacancy__title a").text.strip(),
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
                         com,
                         location,
                         link,
                     ]
                 )
+
+            if (
+                len(
+                    driver.find_elements(
+                        By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                    )
+                )
+                > 0
+            ):
+                driver.find_element(
+                    By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                ).click()
+            else:
+                flag = False
                 break
 
+        except Exception as e:
+            flag = False
 
     driver.quit()
+
     updateDB(key, data)
 
 
