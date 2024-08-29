34abbe98654711231d893249c1ac7a70a2593d01
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:21:58 +0530

    Update the scraper 519

--- scripts/scraper519.py
+++ scripts/scraper519.py
@@ -1,48 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
 from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 519
-    com, url = readUrl(key)
+def main(key, com, url, locations):
+
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
+    if all(loc in locations for loc in ["US", "UK"]):
+        url += "?locationCountry=bc33aa3152ec42d4995f4791a106ed09&locationCountry=29247e57dbaf46fb855b224e03170bc7"
+    elif "US" in locations:
+        url += "?locationCountry=bc33aa3152ec42d4995f4791a106ed09"
+    elif "UK" in locations:
+        url += "?locationCountry=29247e57dbaf46fb855b224e03170bc7"
+
     driver.get(url)
-
-    data = []
-    
     flag = True
-    while flag:
-        time.sleep(4)
-        try:
-            last_height = driver.execute_script("return document.body.scrollHeight")
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                flag = False
-        except:
-            flag = False
-    
-    items = driver.find_elements(By.CSS_SELECTOR, ".search-results.job-tile.job-list-item")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
-        data.append(
-            [
-                item.find_element(By.CSS_SELECTOR, "span.job-tile__title").text.strip(),
-                com,
-                "United Kingdom",
-                link,
-            ]
-        )
+    data = []
 
+    while flag:
+        try:
+            time.sleep(4)
+
+            items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                location = item.find_element(By.CSS_SELECTOR, "dd").text.strip()
+
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+
+            if (
+                len(
+                    driver.find_elements(
+                        By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                    )
+                )
+                > 0
+            ):
+                driver.find_element(
+                    By.CSS_SELECTOR, "button[data-uxi-element-id='next']"
+                ).click()
+            else:
+                flag = False
+                break
+
+        except Exception as e:
+            flag = False
 
     driver.quit()
+
     updateDB(key, data)
 
 
