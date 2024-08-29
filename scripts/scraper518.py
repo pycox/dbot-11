de129f7d23c94a619cbdf922fd5dcd74bfed9ca3
Shiva <shivanand.gupta@outlook.com>
2024-08-29 08:21:53 +0530

    Update the scraper 518

--- scripts/scraper518.py
+++ scripts/scraper518.py
@@ -6,36 +6,37 @@ from utils import readUrl, updateDB
 import time
 
 
-def main():
-    key = 518
-    com, url = readUrl(key)
+def main(key, com, url, locations):
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time.sleep(2)
+    time.sleep(4)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
 
     data = []
     
-    items = driver.find_elements(By.CSS_SELECTOR, ".e-con.e-child .e-con-inner")
-    for item in items:
-        link = item.find_element(By.CSS_SELECTOR, "a.elementor-button.elementor-button-link.elementor-size-sm").get_attribute("href").strip()
-        location = item.find_element(By.CSS_SELECTOR, "p.elementor-heading-title.elementor-size-default").text.strip()
-        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
-            if (str in location):
-                data.append(
-                    [
-                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
-                        com,
-                        location,
-                        link,
-                    ]
-                )
-                break
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".job-list-container .job-container")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, ".position-name").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
 
     driver.quit()
     updateDB(key, data)
+
 
 if __name__ == "__main__":
     main()
