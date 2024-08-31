3982a3b529095f39940039f61f18e3a9bb70f3b4
Shiva <shivanand.gupta@outlook.com>
2024-08-31 09:52:22 +0530

    Add the scraper 577

---
+++ scripts/scraper577.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.osano-cm-dialog__close.osano-cm-close').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, ".Vacancies_vacanciesList__3vXOS"))
+    time.sleep(3)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".Vacancies_vacanciesList__3vXOS .Vacancies_vacancy__E80XU")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".Vacancies_details__2_GDB div:nth-child(1)").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
