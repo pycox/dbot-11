28aa2bac15fb3697cf8a597751c5f16452a425f1
Shiva <shivanand.gupta@outlook.com>
2024-08-31 09:53:53 +0530

    Add the scraper 595

---
+++ scripts/scraper595.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#tarteaucitronPersonalize2').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
+    data = []
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, "a.vacancyItem")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
