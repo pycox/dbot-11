ea0036681524d6bda883f3c0380e59fd16e0fcf1
Shiva <shivanand.gupta@outlook.com>
2024-08-31 09:53:04 +0530

    Add the scraper 584

---
+++ scripts/scraper584.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button.cky-btn.cky-btn-accept').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(4)
+
+    data = []
+    
+    flag = True
+    while flag:
+        time.sleep(4)
+        try:
+            load = driver.find_element(By.CSS_SELECTOR, "a.load-more")
+            if load.get_attribute("style") == "visibility: hidden;":
+                flag = False
+            else:
+                load.click()
+        except Exception:
+            flag = False
+    
+    if "UK" in locations:
+        items = driver.find_elements(By.CSS_SELECTOR, ".career-list li")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            data.append(
+                [
+                    item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
