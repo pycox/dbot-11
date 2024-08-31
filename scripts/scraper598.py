f33c6e5cf5ca49765c01a90e8d8e51af6c952484
Shiva <shivanand.gupta@outlook.com>
2024-08-31 09:54:06 +0530

    Add the scraper 598

---
+++ scripts/scraper598.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler').click()
+    except:
+        print("No Cookie Button")
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(3)
+
+    data = []
+    
+    items = driver.find_elements(By.CSS_SELECTOR, ".position-wrap a")
+    for item in items:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, ".position-location").text.strip()
+        for str in locations:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, ".position-title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
