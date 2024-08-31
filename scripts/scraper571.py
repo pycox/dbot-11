a87c7422a11f95d2e97120ca1eb23fa20d82bc82
Shiva <shivanand.gupta@outlook.com>
2024-08-31 09:51:53 +0530

    Add the scraper 571

---
+++ scripts/scraper571.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from utils import readUrl, updateDB
+import time
+
+
+def main(key, com, url, locations):
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(3)
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.cky-btn.cky-btn-accept').click()
+    except:
+        print("No Cookie Button")
+
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "#Trigger_Careers_View").click()
+
+    time.sleep(3)
+
+    data = []
+    
+    if "UK" in locations:
+        flag = True
+        while flag:
+            time.sleep(4)
+            try:
+                last_height = driver.execute_script("return document.body.scrollHeight")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(4)
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    flag = False
+            except:
+                flag = False
+        
+        items = driver.find_elements(By.CSS_SELECTOR, ".card.job-card a.VF-job-title-link")
+        for item in items:
+            link = item.get_attribute("href").strip()
+            data.append(
+                [
+                    item.text.strip(),
+                    com,
+                    "UK",
+                    link,
+                ]
+            )
+
+    driver.quit()
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
